34cae25c2964c39ff04267ffb50ba927ab9ca5a3
jim_lin <jim_lin@wistron.com>
2024-08-26 19:15:06 +0800

    [UI/SA]  To fix PIMS-291461 for auto color issue.

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
@@ -655,6 +655,7 @@ namespace DDPM.SA.Plugins.User.DeviceManager
                         MonitorBorkerWin = new MainWindow(this, m);
 
                         MonitorBorkerWin.Show();
+                        MonitorBorkerWin.Set_AUTO_ColorPresetConfig(true);
                     }
                     else
                     {
