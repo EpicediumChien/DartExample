a98a7bc527793e9cb9cde524ff41a284ac15f2d1
Dean Yang(WISTRON) <dean_yang@wistron.com>
2024-08-30 10:49:10 +0800

    try to fix conflict

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.CLIProxy/CLIProxyPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.CLIProxy/CLIProxyPlugin.cs
@@ -382,6 +382,14 @@ namespace DDPM.SA.Plugin.User.CLIManager
                     return;
                 }
 
+                List<string> peripheral_DeviceType = new List<string>()
+                {
+                    "MOUSE",
+                    "KEYBOARD",
+                    "DOCK",
+                    "HEADSET",
+                };
+
                 //Do command line action
                 CLIEventResult? cliEventResult;
                 CommandLineInput commandLineInput = e.commandLineInput;
@@ -398,10 +406,11 @@ namespace DDPM.SA.Plugin.User.CLIManager
                         return;
                     }
                 }
-                else if (commandLineInput.PluginsType.Equals("MOUSE") || 
-                         commandLineInput.PluginsType.Equals("KEYBOARD") || 
-                         commandLineInput.PluginsType.Equals("DOCK") || 
-                         commandLineInput.PluginsType.Equals("HEADSET"))
+                else if (peripheral_DeviceType.FindIndex( x => x.Equals(commandLineInput.PluginsType) ) >= 0)
+                //commandLineInput.PluginsType.Equals("MOUSE") || 
+                //commandLineInput.PluginsType.Equals("KEYBOARD") || 
+                //commandLineInput.PluginsType.Equals("DOCK") || 
+                //commandLineInput.PluginsType.Equals("HEADSET")) //please edit upper table "peripheral_DeviceType" if new device supported
                 {
                     if (_CLIPeripherals != null)
                     {
