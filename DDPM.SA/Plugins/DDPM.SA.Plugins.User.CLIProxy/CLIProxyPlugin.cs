6b37311047e26c51ad176a5e90985cba50da23e8
Dean Yang(WISTRON) <dean_yang@wistron.com>
2024-08-30 10:51:59 +0800

    try to fix conflict

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.CLIProxy/CLIProxyPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.CLIProxy/CLIProxyPlugin.cs
@@ -406,11 +406,9 @@ namespace DDPM.SA.Plugin.User.CLIManager
                         return;
                     }
                 }
+                //please edit upper table "peripheral_DeviceType" if new device supported
+                //else if (commandLineInput.PluginsType.Equals("MOUSE") || commandLineInput.PluginsType.Equals("KEYBOARD") || commandLineInput.PluginsType.Equals("DOCK") || commandLineInput.PluginsType.Equals("HEADSET"))
                 else if (peripheral_DeviceType.FindIndex( x => x.Equals(commandLineInput.PluginsType) ) >= 0)
-                //commandLineInput.PluginsType.Equals("MOUSE") || 
-                //commandLineInput.PluginsType.Equals("KEYBOARD") || 
-                //commandLineInput.PluginsType.Equals("DOCK") || 
-                //commandLineInput.PluginsType.Equals("HEADSET")) //please edit upper table "peripheral_DeviceType" if new device supported
                 {
                     if (_CLIPeripherals != null)
                     {
