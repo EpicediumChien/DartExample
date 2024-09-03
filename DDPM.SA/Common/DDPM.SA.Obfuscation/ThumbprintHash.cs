512c102792eb7283b94da49b2c8f4f1a4357a57b
Dean Yang(WISTRON) <dean_yang@wistron.com>
2024-09-03 15:49:49 +0800

    [SA] add Qisda NKVM thunmprint

--- DDPM.SA/Common/DDPM.SA.Obfuscation/ThumbprintHash.cs
+++ DDPM.SA/Common/DDPM.SA.Obfuscation/ThumbprintHash.cs
@@ -46,11 +46,20 @@
 
         private static readonly byte[] WST2_Hash = new byte[]
         {
-            //Wistron2 Thumbprint : 841c87c9f5a679dcdba8a9c7f743847d157cd598
+            //Wistron2 Thumbprint : 841c87c9f5a679dcdba8a9c7f743847d157cd598                                    
             //84 1c 87 c9 f5 a6 79 dc db a8
             //a9 c7 f7 43 84 7d 15 7c d5 98
             0x84, 0x1c, 0x87, 0xc9, 0xf5, 0xa6, 0x79, 0xdc, 0xdb, 0xa8,
             0xa9, 0xc7, 0xf7, 0x43, 0x84, 0x7d, 0x15, 0x7c, 0xd5, 0x98
+        };
+
+        private static readonly byte[] QDA1_Hash = new byte[]
+        {
+            //Qisda NKVM Thumbprint : ca7356d58f31d1a4f1e94c37e73c5bd2f894d5ac
+            //ca 73 56 d5 8f 31 d1 a4 f1 e9
+            //4c 37 e7 3c 5b d2 f8 94 d5 ac
+            0xca, 0x73, 0x56, 0xd5, 0x8f, 0x31, 0xd1, 0xa4, 0xf1, 0xe9,
+            0x4c, 0x37, 0xe7, 0x3c, 0x5b, 0xd2, 0xf8, 0x94, 0xd5, 0xac
         };
 
         public static byte[][] certificateHash = {
