ebae3446571ac0da5ef7350042c14edfc91ea0c5
JasonLin <jasonlin3@wits.com>
2024-09-13 14:20:47 +0800

    [SA][20240913]add NKVM CLI get

---
+++ DDPM.SA/Common/DDPM.SA.Common/Display/KVMInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace DDPM.SA.Common
+{
+    internal class KVMInfo
+    {
+    }
+
+    public class PCsInfo
+    {
+        public string InputType { get; set; }
+        public string InputName { get; set; }
+        public string USBUpstream { get; set; }
+    }
+
+    public class NKVMRespone
+    {
+        public string CLIName { get; set; }
+        public string Respone { get; set; }
+    }
+}
