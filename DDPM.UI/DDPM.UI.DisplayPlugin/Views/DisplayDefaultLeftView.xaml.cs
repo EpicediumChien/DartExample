a6454e4e956e6c7b40cb54c533ac13ddd4416a6e
Dean Yang(WISTRON) <dean_yang@wistron.com>
2024-09-19 08:24:10 +0800

    [UI] add webcam and mouse lock event

--- DDPM.UI/DDPM.UI.DisplayPlugin/Views/DisplayDefaultLeftView.xaml.cs
+++ DDPM.UI/DDPM.UI.DisplayPlugin/Views/DisplayDefaultLeftView.xaml.cs
@@ -24,20 +24,20 @@ namespace DDPM.UI.Plugin.DisplayPlugin.Views
             if (DdpmCommonHelper.DeviceManagerSA != null)
             {
                 DdpmCommonHelper.DeviceManagerSA.ITSettingsActionEvent += DeviceManagerSA_ITSettingsActionEvent;
-            }
 
-            DDPMSettings data = DdpmCommonHelper.DeviceManagerSA.ReloadAppConfigData().Result;
-            if (data != null)
-            {
-                if (data.LockSettings.Lock_Setting_RestoreDefaults)
+                DDPMSettings data = DdpmCommonHelper.DeviceManagerSA.ReloadAppConfigData().Result;
+                if (data != null)
                 {
-                    RestoreLockIcon.Visibility = Visibility.Visible;
-                    txtRestore.IsEnabled = false;
-                }
-                else
-                {
-                    txtRestore.IsEnabled = !data.LockSettings.Lock_Display_RestoreFactoryDefaults;
-                    RestoreLockIcon.Visibility = data.LockSettings.Lock_Display_RestoreFactoryDefaults ? Visibility.Visible : Visibility.Collapsed;
+                    if (data.LockSettings.Lock_Setting_RestoreDefaults)
+                    {
+                        RestoreLockIcon.Visibility = Visibility.Visible;
+                        txtRestore.IsEnabled = false;
+                    }
+                    else
+                    {
+                        txtRestore.IsEnabled = !data.LockSettings.Lock_Display_RestoreFactoryDefaults;
+                        RestoreLockIcon.Visibility = data.LockSettings.Lock_Display_RestoreFactoryDefaults ? Visibility.Visible : Visibility.Collapsed;
+                    }
                 }
             }
         }
