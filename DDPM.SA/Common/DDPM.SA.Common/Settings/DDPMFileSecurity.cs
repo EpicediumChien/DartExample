41743d8ee696b9287aba126a990de4e77fa0798d
Dean Yang(WISTRON) <dean_yang@wistron.com>
2024-09-20 18:18:07 +0800

    [SA] by Wendy's comment to update HMAC method

--- DDPM.SA/Common/DDPM.SA.Common/Settings/DDPMFileSecurity.cs
+++ DDPM.SA/Common/DDPM.SA.Common/Settings/DDPMFileSecurity.cs
@@ -73,7 +73,7 @@ namespace DDPM.SA.Common.Settings
         }
 
         //need system privilege to query this string
-        public static string AppAccessInfo { get; } = SettingsAccess.AppAccessInfo;
+        //public static string AppAccessInfo { get; } = SettingsAccess.AppAccessInfo;
 
         /// <summary>
         /// Apply DDPM data security [Write settings]
@@ -120,7 +120,8 @@ namespace DDPM.SA.Common.Settings
                     info = "DDPM AccessInfo value is abnormal";
                     return false;
                 }
-                signature = SettingsAccess.GenerateAccessString(Encoding.UTF8.GetBytes(accessInfo), serialized_string);
+                //signature = SettingsAccess.GenerateAccessString(Encoding.UTF8.GetBytes(accessInfo), serialized_string);
+                signature = SettingsAccess.ComputeAccessInfo2(Encoding.UTF8.GetBytes(accessInfo), serialized_string);
             }
             catch (Exception e)
             {
@@ -272,7 +273,9 @@ namespace DDPM.SA.Common.Settings
                 //byte[] body_array = Encoding.UTF8.GetBytes(modifiedJson);
                 //byte[] sign = GetSHA512(body_array, 0, body_array.Length);
                 //cal_sign = Encoding.UTF8.GetString(sign);//target for comparison
-                cal_sign = SettingsAccess.GenerateAccessString(Encoding.UTF8.GetBytes(accessInfo), modifiedJson);
+
+                //cal_sign = SettingsAccess.GenerateAccessString(Encoding.UTF8.GetBytes(accessInfo), modifiedJson);
+                cal_sign = SettingsAccess.ComputeAccessInfo2(Encoding.UTF8.GetBytes(accessInfo), modifiedJson);
                 if (string.IsNullOrEmpty(cal_sign))
                 {
                     info = "Null signature from hash calculation";
