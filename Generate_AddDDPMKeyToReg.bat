dd6750666a6d7e377c7ab261c68242ba00e21f93
ELIE_LIAO <ELIE_LIAO@WISTRON.COM>
2024-09-24 08:35:40 +0800

    [SA] Remove remark of TestSetPrimaryMouseButton code, update generate uninstall reg batch.

--- Generate_AddDDPMKeyToReg.bat
+++ Generate_AddDDPMKeyToReg.bat
@@ -1,15 +1,6 @@
-:: GenerateReg.bat v4 by Elie_Liao
+:: Generate_AddDDPMKeyToReg.bat v4 by Elie_Liao
 :: -------------------------------------------------
-:: This batch program will generate DDPMSA.reg file base on current directory.
-:: You should run this batch program when you change the base dir of the source code.
-:: And then import the "DDPMSA.reg" into your Windows Registry.
-:: [v4] 2024-8-28 Elie_Liao
-:: fixed wrong name (DDPM.SA.Plugins.User.ActionsManger.)
-:: Add DDPM.SA.Plugins.User.DTPProxy.dll to SubAgent.Uer
-:: [v3] 2024-6-27 Robert_Lin
-:: Add EasyArrangeService plugin to SubAgent.User registry
-:: [v2] 2024-5-23 Robert_Lin
-:: Add PipPbpManager Plugin Registery keys
+:: This is used to generate a Regkey for configuration files. 
 :: -
 SET CurDir=%CD%
 REM - Replace "\" with "\\"
@@ -23,7 +14,7 @@ SET OutFile=AddDDPMKeyToReg.reg
 echo Windows Registry Editor Version 5.00 >%OutFile%
 echo.>>%OutFile%
 :: This section is used to generate secret key and version info, test purpose
-echo [HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{21A24609-08A2-423E-80DE-4D33A933F1A1}]>>%OutFile%
+echo [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{21A24609-08A2-423E-80DE-4D33A933F1A1}]>>%OutFile%
 echo "DisplayName"="Dell Display and Peripheral Manager">>%OutFile%
 echo "DisplayVersion"="2.0.0.40">>%OutFile%
 echo "InstallLocation"="C:\\Program Files\\Dell\\Dell Display and Peripheral Manager">>%OutFile%
