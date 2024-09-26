95ceb9afacec249b4c7e76f7d57bafd72d642e8f
jesper_lin <jesper_lin@wistron.com>
2024-09-26 21:25:01 +0800

    [UI] SDL Input Validation for Display - Input Name

--- DDPM.UI/DDPM.UI.Module.InputSource/InputSourceRightView.xaml.cs
+++ DDPM.UI/DDPM.UI.Module.InputSource/InputSourceRightView.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace DDPM.UI.Module.InputSource
@@ -66,6 +67,39 @@ namespace DDPM.UI.Module.InputSource
             vm.inputList[vm.items[(int)tb.Tag].InputType].InputName = tb.Text;
             //DdpmCommonHelper.DeviceManagerSA.SetInputName(vm.items[(int)tb.Tag].InputType, vm.items[(int)tb.Tag].InputName);
             bool b = DdpmCommonHelper.DeviceManagerSA.SetInputSourcelist(vm.InputSourceModule.SelectedHomeDevice.MonitorInfo, vm.inputList).Result;
+        }
+
+        private void KeyDown_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            KeyConverter kc = new KeyConverter();
+            var str = kc.ConvertToString(e.Key);
+
+            if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+            {
+                if (Keyboard.IsKeyDown(Key.D2))
+                {
+                    // Handle "@"
+                }
+                else
+                {
+                    e.Handled = true;
+                }
+            }
+            else if (Keyboard.IsKeyDown(Key.D0) || Keyboard.IsKeyDown(Key.D1) || Keyboard.IsKeyDown(Key.D2) || Keyboard.IsKeyDown(Key.D3) || Keyboard.IsKeyDown(Key.D4) ||
+                Keyboard.IsKeyDown(Key.D5) || Keyboard.IsKeyDown(Key.D6) || Keyboard.IsKeyDown(Key.D7) || Keyboard.IsKeyDown(Key.D8) || Keyboard.IsKeyDown(Key.D9) ||
+                Keyboard.IsKeyDown(Key.A) || Keyboard.IsKeyDown(Key.B) || Keyboard.IsKeyDown(Key.C) || Keyboard.IsKeyDown(Key.D) || Keyboard.IsKeyDown(Key.E) ||
+                Keyboard.IsKeyDown(Key.F) || Keyboard.IsKeyDown(Key.G) || Keyboard.IsKeyDown(Key.H) || Keyboard.IsKeyDown(Key.I) || Keyboard.IsKeyDown(Key.J) ||
+                Keyboard.IsKeyDown(Key.K) || Keyboard.IsKeyDown(Key.L) || Keyboard.IsKeyDown(Key.M) || Keyboard.IsKeyDown(Key.N) || Keyboard.IsKeyDown(Key.O) ||
+                Keyboard.IsKeyDown(Key.P) || Keyboard.IsKeyDown(Key.Q) || Keyboard.IsKeyDown(Key.R) || Keyboard.IsKeyDown(Key.S) || Keyboard.IsKeyDown(Key.T) ||
+                Keyboard.IsKeyDown(Key.U) || Keyboard.IsKeyDown(Key.V) || Keyboard.IsKeyDown(Key.W) || Keyboard.IsKeyDown(Key.X) || Keyboard.IsKeyDown(Key.Y) ||
+                Keyboard.IsKeyDown(Key.Z) || Keyboard.IsKeyDown(Key.OemMinus))
+            {
+                // Handle 0-9, a-z, A-Z, " ", "-" 
+            }
+            else
+            {
+                e.Handled = true;
+            }
         }
     }
 
