888d3c34176210b8174fd81ec8bc7b62a1fa5f3e
jarvis_shih <jarvis_shih@wistron.com>
2024-09-27 20:58:47 +0800

    [SA] Fix Brightness/Contrast Global Lock Issue

--- DDPM.UI/DDPM.UI.Module.Brightness/BrightnessViewModel.cs
+++ DDPM.UI/DDPM.UI.Module.Brightness/BrightnessViewModel.cs
@@ -603,10 +603,13 @@ namespace DDPM.UI.Module.Brightness
                 //Lock/unlock mask and tabstop init here
                 DDPMSettings data = DdpmCommonHelper.DeviceManagerSA.ReloadAppConfigData().Result;//Be careful if spend much time here
                 Update_ALSLockStatus(data.LockSettings.Lock_Display_AutoBriTemp);
+                Update_BriContLockStatus(data.LockSettings.Lock_Display_BriCont);
+                Update_SyncLockStatus((data.LockSettings.Lock_Display_BriCont || data.LockSettings.Lock_Display_ColorPreset || data.LockSettings.Lock_Display_AutoBriTemp));
                 //ex: vm.LockMaskVisible = data.LockSettings.Lock_Display_BriCont ? Visibility.Visible : Visibility.Collapsed;
                 //Read user default lock value, these values are synced from IT lock event
                 Trace.WriteLine($"[SettingsPage] Apply Brightness/Contrast(Lock) : {data.LockSettings.Lock_Display_BriCont}");
                 Trace.WriteLine($"[SettingsPage] Apply Auto Brightness(Lock) : {data.LockSettings.Lock_Display_AutoBriTemp}");
+                Trace.WriteLine($"[SettingsPage] Apply Synchroniz Button(Lock) : {(data.LockSettings.Lock_Display_BriCont || data.LockSettings.Lock_Display_ColorPreset || data.LockSettings.Lock_Display_AutoBriTemp)}");
             }
         }
 
@@ -2293,6 +2296,16 @@ namespace DDPM.UI.Module.Brightness
                 IsPrimaryMonitorSyncLockMask = Visibility.Collapsed;
                 IsAutoBrightnessRangeLevelStringLockMask = Visibility.Collapsed;
             }
+        }
+
+        public void Update_BriContLockStatus(bool value)
+        {
+            LockMaskVisible = value ? Visibility.Visible : Visibility.Collapsed;
+            TabSTOP = value ? "None" : "Cycle";
+        }
+        public void Update_SyncLockStatus(bool value)
+        {
+            synchronizeLock = value ? Visibility.Visible : Visibility.Collapsed;
         }
 
         private Visibility _isAutoBrightnessLockMask = Visibility.Collapsed;
