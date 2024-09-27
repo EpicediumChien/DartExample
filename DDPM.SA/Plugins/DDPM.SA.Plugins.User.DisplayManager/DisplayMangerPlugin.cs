d5a5b7acaec8a940f8763a9bf7ba4e67590e1494
brucechuang <brucechuang@wits.com>
2024-09-27 19:49:54 +0800

    [SA] Undo test change

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.DisplayManager/DisplayMangerPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.DisplayManager/DisplayMangerPlugin.cs
@@ -3408,7 +3408,7 @@ namespace DDPM.SA.Plugins.User.DisplayManager
                                         break;
                                     }
                                 }
-                                if (newVersion >= oldVersion)
+                                if (newVersion > oldVersion)
                                 {
                                     ret.Firmwares.Add(firmwares_item);
                                 }
