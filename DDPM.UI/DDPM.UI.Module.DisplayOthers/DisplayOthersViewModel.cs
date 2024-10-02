89608edb8d3ad83bb4bf5843563d3bde15b28213
jesper_lin <jesper_lin@wistron.com>
2024-10-02 23:41:29 +0800

    [SA][UI] Input validation- call IsFilePathValid and IsPathSymbolicLinked for FilePath check.

--- DDPM.UI/DDPM.UI.Module.DisplayOthers/DisplayOthersViewModel.cs
+++ DDPM.UI/DDPM.UI.Module.DisplayOthers/DisplayOthersViewModel.cs
@@ -239,7 +239,7 @@ namespace DDPM.UI.Module.DisplayOthers
             {
                 string filename = saveFileDialog.FileName;
                 string info = string.Empty;
-                if (!DDPM.SA.Common.Security.InputHelper.InputValidation_FilePathFileName(filename, true, out info))
+                if (!DDPM.SA.Common.Security.InputHelper.InputValidation_FilePathFileName(filename, false, out info))
                 {
                     IsBusy = false;
                     OnPropertyChanged("IsBusy");
