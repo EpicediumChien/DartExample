67ec7d90e70bae06c33d0eac5be06da4a00bd704
Dean Yang(WISTRON) <dean_yang@wistron.com>
2024-10-04 20:21:23 +0800

    [SA][CLI] add InAppAutoBrightnessColor and it is compatible with InAppAtoBriTemp.

--- DDPM.SA/Common/DDPM.SA.Common/CLI/ICLICommandTable.cs
+++ DDPM.SA/Common/DDPM.SA.Common/CLI/ICLICommandTable.cs
@@ -36,6 +36,7 @@ namespace DDPM.SA.Common
             "INAPPUPDATE",              //InAppUpdate               DDPMW-1329/1330
             "INAPPBRICONT",             //InAppBriCont              DDPMW-1342/1343
             "INAPPAUTOBRITEMP",         //InAppAutoBriTemp          DDPMW-1341
+            "INAPPAUTOBRIGHTNESSCOLOR",//1004 InAppAutoBrightnessColor DDPMW1341, same as InAppAutoBriTemp
             "INAPPRESTOREDEFAULTS",     //InAppRestoreDefaults      DDPMW-1333
             "INAPPRESTORE",             //InAppRestore              Same as InAppRestoreDefaults
             "RESTOREFACTORYDEFAULTS",   //RestoreFactoryDefaults    DDPMW-2013/2014/2015/2111/2114
