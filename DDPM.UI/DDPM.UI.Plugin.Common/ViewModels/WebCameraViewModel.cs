593604d0948d5241996cd998d76ae5a742adfb3c
Hess Cheng <hesscheng@gmail.com>
2024-10-05 01:16:04 +0800

    241005

--- DDPM.UI/DDPM.UI.Plugin.Common/ViewModels/WebCameraViewModel.cs
+++ DDPM.UI/DDPM.UI.Plugin.Common/ViewModels/WebCameraViewModel.cs
@@ -141,6 +141,7 @@ namespace DDPM.UI.Plugin.ViewModels
                 FOV_IsSelected[j] = false;
             }
             FOV_IsSelected[index] = true;
+            WebcamSettings.ExportWebcamSettings(WebcamSettings, Model);
             OnPropertyChanged(nameof(FOV_IsSelected));
         }
 
@@ -1154,7 +1155,7 @@ namespace DDPM.UI.Plugin.ViewModels
                     DdpmCommonHelper.DeviceManagerSA!.SetAutoFramingFrameSize(CurrentDeviceInfo!.ID.ToString(), (int)value);
                     break;
                 case "FieldOfView":
-                    DdpmCommonHelper.DeviceManagerSA!.SetPriority(CurrentDeviceInfo!.ID.ToString(), (int)value);
+                    DdpmCommonHelper.DeviceManagerSA!.SetFieldOfView(CurrentDeviceInfo!.ID.ToString(), (int)value);
                     break;
             }
             OnPropertyChanged(property);
