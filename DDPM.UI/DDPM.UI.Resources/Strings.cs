d70f6e1f86c72382dfa0a9d53b93b54c6ab54cff
Hess Cheng <HessCheng@wits.com>
2024-10-19 12:34:02 +0800

    241019

--- DDPM.UI/DDPM.UI.Resources/Strings.cs
+++ DDPM.UI/DDPM.UI.Resources/Strings.cs
@@ -570,6 +570,17 @@ namespace DDPM.UI.Common
         public static readonly string ImpExp_WarningMsg0 = GetString("ImpExp_WarningMsg.0");
         public static readonly string ImpExp_WarningMsg1 = GetString("ImpExp_WarningMsg.1");
 
+        // Pair/Unpair
+        public static readonly string Caption = "Are you sure?";
+        public static readonly string MessageMouse = "Unpairing your mouse can limit your ability to use this computer. Make sure you have an alternative mouse setup before unpairing.";
+        public static readonly string MessageKeyboard = "Unpairing your keyboard can limit your ability to use this computer. Make sure you have an alternative keyboard setup before unpairing.";
+        public static readonly string MessagePen = "Unpairing your pen can limit your ability to use this computer. Make sure you have an alternative pen setup before unpairing.";
+        public static readonly string MessageHeadset = "This will unpair your headset from its USB wireless receiver. You can still pair and use the headset on this system via Bluetooth. If required, you may pair the headset back to the wireless receiver from + icon on top right of the home screen of [NAME] .";
+        public static readonly string Continue = "Continue";
+        public static readonly string Imcompatible = "This device is not compatible with";
+        public static readonly string PenAlreadyPaired = "This device is already paired. Unpair the device first if you would like to pair again.";
+        public static readonly string PairYourPen = "Pair your pen";
+        public static readonly string PairYourPenMessage = "Would you like to pair your pen with your system?";
         // Display Restore to Default
         public static readonly string DisplayDefault0 = GetString("DisplayDefault.0");
     }
