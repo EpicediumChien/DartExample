c7e0f276c8234afb4cf88ee227c43e90041cffe6
brucechuang <brucechuang@wits.com>
2024-10-24 14:57:40 +0800

    [SA]

--- DDPM.SA/DDPM.Subagent.User/Program.cs
+++ DDPM.SA/DDPM.Subagent.User/Program.cs
@@ -42,7 +42,7 @@ namespace DDPM.Subagent.User
         /// <summary>
         ///     A name for your product.
         /// </summary>
-        private const string ProductName = "DDPMSubagentUser";
+        private const string ProductName = "DDPM Subagent User";
 
         /// <summary>
         ///     Service name for the product. Even if you are not running as a service always provide this because DCF provides you
