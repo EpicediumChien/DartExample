6b43aca69262ae135732b0e8449bbf9d1422481a
jarvis_shih <jarvis_shih@wistron.com>
2024-10-30 20:28:10 +0800

    [SA] Update Telementry

---
+++ DDPM.SA/Common/DDPM.SA.Common/Telemetry/DeviceTypeConnectedBasic.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace DDPM.SA.Common
+{
+    public class DeviceTypeConnectedBasic
+    {
+        public List<string> DisplayServiceTag { get; set; }
+
+        public List<string> DisplayModelname { get; set; }
+
+        public List<string> ConnectedType { get; set; }
+
+        public DeviceTypeConnectedBasic()
+        {
+            ConnectedType = new List<string>();
+            DisplayModelname = new List<string>();
+            DisplayServiceTag = new List<string>();
+        }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+
+    public class DeviceTypeConnected_VideoPortInUsed : DeviceTypeConnectedBasic
+    {
+        public int totalnum { get; set; }
+    }
+}
