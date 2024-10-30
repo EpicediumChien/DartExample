d4d7c8fb86321a9a4c3654c5b7109870e2329621
Hess Cheng <HessCheng@wits.com>
2024-10-30 18:13:34 +0800

    add consent screen

---
+++ DDPM.UI/DDPM.UI.Plugin.Common/ConsentModalDialog.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DDPM.UI.Resources.Helper;
+
+namespace DDPM.UI.Plugin.Common
+{
+    /// <summary>
+    /// ConsentModalDialog.xaml 的互動邏輯
+    /// </summary>
+    public partial class ConsentModalDialog : Window
+    {
+        private string PrivacyUrl = "https://www.dell.com/learn/us/en/uscorp1/policies-privacy-country-specific-privacy-policy";
+
+        public ConsentModalDialog(double width, double height)
+        {
+            InitializeComponent();
+            this.Width = width;
+            this.Height = height;
+            txtYes.Text = LangHelper.Instance["ConsentYes"];
+            txtNo.Text = LangHelper.Instance["ConsentNo"];
+            txtCaption.Text = LangHelper.Instance["Consent.1"];
+            txtCaption2.Text = LangHelper.Instance["AppName"];
+            txt1.Text = LangHelper.Instance["Consent.2"];
+            txt2.Text = LangHelper.Instance["Analytics.2"];
+        }
+
+        private void No_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        private void Yes_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void OpenPrivacy(object sender, MouseButtonEventArgs e)
+        {
+            DDPM.SA.Common.Settings.DDPMFileSecurity.StartProcessSafely(
+                null,
+                new ProcessStartInfo
+                {
+                    FileName = PrivacyUrl,
+                    UseShellExecute = true
+                });
+        }
+    }
+}
--- DDPM.UI/DDPM.UI.Resources/Resources.Designer.cs
+++ DDPM.UI/DDPM.UI.Resources/Resources.Designer.cs
@@ -592,6 +592,15 @@ namespace DDPM.UI.Resources {
         }
         
         /// <summary>
+        ///   查詢類似 Dell Display and Peripheral Manager 的當地語系化字串。
+        /// </summary>
+        internal static string AppName {
+            get {
+                return ResourceManager.GetString("AppName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   查詢類似 Assign Keystroke 的當地語系化字串。
         /// </summary>
         internal static string AssignKeystroke {
@@ -1353,6 +1362,42 @@ namespace DDPM.UI.Resources {
         internal static string Confirm {
             get {
                 return ResourceManager.GetString("Confirm", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   查詢類似 Help us improve 的當地語系化字串。
+        /// </summary>
+        internal static string Consent_1 {
+            get {
+                return ResourceManager.GetString("Consent.1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   查詢類似 Help Dell improve its products and services by automatically sending diagnostics and usage data. Learn more about  的當地語系化字串。
+        /// </summary>
+        internal static string Consent_2 {
+            get {
+                return ResourceManager.GetString("Consent.2", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   查詢類似 No, I don&apos;t consent 的當地語系化字串。
+        /// </summary>
+        internal static string ConsentNo {
+            get {
+                return ResourceManager.GetString("ConsentNo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   查詢類似 Yes, I consent 的當地語系化字串。
+        /// </summary>
+        internal static string ConsentYes {
+            get {
+                return ResourceManager.GetString("ConsentYes", resourceCulture);
             }
         }
         
