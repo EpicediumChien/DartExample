5f5c5c77a417043d630aaa000c6d09242887bfb7
jim_lin <jim_lin@wistron.com>
2024-10-30 18:21:35 +0800

    [UI/SA] Telemetry feature  for Color Preset Manual and Color Preset Auto and Niightlight : <on/off>

--- DDPM.SA/Common/DDPM.SA.Common/Telemetry/Displaysettings_Function.cs
+++ DDPM.SA/Common/DDPM.SA.Common/Telemetry/Displaysettings_Function.cs
@@ -72,5 +72,75 @@ namespace DDPM.SA.Common
             }
             return rt;
         }
+
+        public bool Send_Color_Preset_Manual_Telementry(ITelementryScheduler plugin, MonitorInfo monitorInfo, string val, string currentResolution, string maxResolution)
+        {
+            var rt = false;
+            if (monitorInfo != null)
+            {
+                var TelemetryDta_Color_Preset_Manual = new Displaysettings_Color_Preset_Manual();
+                TelemetryDta_Color_Preset_Manual.Color_Preset_Manual = val;
+                TelemetryDta_Color_Preset_Manual.CommunicationPath = "Video";
+                TelemetryDta_Color_Preset_Manual.MonitorName = monitorInfo.AliasDeviceName;
+                TelemetryDta_Color_Preset_Manual.D_Ctrl = monitorInfo.D_Ctrl;
+                TelemetryDta_Color_Preset_Manual.SupplierID = monitorInfo.SupplierID;
+                TelemetryDta_Color_Preset_Manual.FirmwareVersion = monitorInfo.FwVersion;
+                TelemetryDta_Color_Preset_Manual.DisplayModelname = monitorInfo.modelName;
+                TelemetryDta_Color_Preset_Manual.DisplayServiceTag = monitorInfo.edid.ServiceTag;
+                TelemetryDta_Color_Preset_Manual.DsiplayResolution = currentResolution;
+                TelemetryDta_Color_Preset_Manual.MaxDisplayResolution = maxResolution;
+
+                if (plugin != null)
+                    rt = plugin.ReceiveTelemetryInfo("Displaysettings", TelemetryDta_Color_Preset_Manual.ToJson(), Telementry_Frequency.RealTime).Result;
+            }
+            return rt;
+        }
+
+        public bool Send_Color_Preset_Auto_Telementry(ITelementryScheduler plugin, MonitorInfo monitorInfo, string val, string currentResolution, string maxResolution)
+        {
+            var rt = false;
+            if (monitorInfo != null)
+            {
+                var TelemetryDta_Color_Preset_Auto = new Displaysettings_Color_Preset_Auto();
+                TelemetryDta_Color_Preset_Auto.Color_Preset_Auto = val;
+                TelemetryDta_Color_Preset_Auto.CommunicationPath = "Video";
+                TelemetryDta_Color_Preset_Auto.MonitorName = monitorInfo.AliasDeviceName;
+                TelemetryDta_Color_Preset_Auto.D_Ctrl = monitorInfo.D_Ctrl;
+                TelemetryDta_Color_Preset_Auto.SupplierID = monitorInfo.SupplierID;
+                TelemetryDta_Color_Preset_Auto.FirmwareVersion = monitorInfo.FwVersion;
+                TelemetryDta_Color_Preset_Auto.DisplayModelname = monitorInfo.modelName;
+                TelemetryDta_Color_Preset_Auto.DisplayServiceTag = monitorInfo.edid.ServiceTag;
+                TelemetryDta_Color_Preset_Auto.DsiplayResolution = currentResolution;
+                TelemetryDta_Color_Preset_Auto.MaxDisplayResolution = maxResolution;
+
+                if (plugin != null)
+                    rt = plugin.ReceiveTelemetryInfo("Displaysettings", TelemetryDta_Color_Preset_Auto.ToJson(), Telementry_Frequency.RealTime).Result;
+            }
+            return rt;
+        }
+
+        public bool Send_NightLightStatus_Telementry(ITelementryScheduler plugin, MonitorInfo monitorInfo, string val, string currentResolution, string maxResolution)
+        {
+            var rt = false;
+            if (monitorInfo != null)
+            {
+                var TelemetryDta_NightLightStatus = new Displaysettings_NightLightStatus();
+                TelemetryDta_NightLightStatus.NightLightStatus = val;
+                TelemetryDta_NightLightStatus.CommunicationPath = "Video";
+                TelemetryDta_NightLightStatus.MonitorName = monitorInfo.AliasDeviceName;
+                TelemetryDta_NightLightStatus.D_Ctrl = monitorInfo.D_Ctrl;
+                TelemetryDta_NightLightStatus.SupplierID = monitorInfo.SupplierID;
+                TelemetryDta_NightLightStatus.FirmwareVersion = monitorInfo.FwVersion;
+                TelemetryDta_NightLightStatus.DisplayModelname = monitorInfo.modelName;
+                TelemetryDta_NightLightStatus.DisplayServiceTag = monitorInfo.edid.ServiceTag;
+                TelemetryDta_NightLightStatus.DsiplayResolution = currentResolution;
+                TelemetryDta_NightLightStatus.MaxDisplayResolution = maxResolution;
+
+                if (plugin != null)
+                    rt = plugin.ReceiveTelemetryInfo("Displaysettings", TelemetryDta_NightLightStatus.ToJson(), Telementry_Frequency.RealTime).Result;
+            }
+            return rt;
+        }
+
     }
 }
