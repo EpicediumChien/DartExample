8815b5fe6a5b84a001b1df0cf48f71dbaa52fbe6
Eric Chien <EricChien@wits.com>
2024-10-31 15:10:56 +0800

    Update test cases to json files

--- DDPM.CMA.Tester/DDPM.CMA.Tester/CMAAgent.cs
+++ DDPM.CMA.Tester/DDPM.CMA.Tester/CMAAgent.cs
@@ -10,6 +10,19 @@ using System.Text.Json;
 
 namespace DDPM.CMA.Tester
 {
+    public static class StringExtensions
+    {
+        public static string PlainJsonString(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            return input.Replace("\n", "").Replace("\r", "").Replace(" ", "");
+        }
+    }
+
     public class CMAAgent
     {
         #region Fields
@@ -120,32 +133,40 @@ namespace DDPM.CMA.Tester
         {
             Boolean isRunning = true;
 
-            
-            string jsonacthours = @"{""sid"":""1727362336"",""req"":[{""tid"":1,""active"":""get"",""devicetype"":""DISPLAY"",""command"":""ActiveHours"",""options"":{}}]}";
-            // string jsonActHours = await ReadJsonFileAsync("ActHours");
-            string jsongetdisplaymulti = @"{""sid"":""1727362335"",""req"":[{""tid"":1,""active"":""get"",""devicetype"":""DISPLAY"",""command"":""ActiveHours"",""options"":{}},{""tid"":2,""active"":""get"",""devicetype"":""DISPLAY"",""command"":""Brightnesslevel"",""options"":{}}]}";
+            List<Task<string>> loadJsonTasks = new List<Task<string>>() {
+                ReadJsonFileAsync("ActHours"),
+                ReadJsonFileAsync("GetDisplayMulti"),
+                ReadJsonFileAsync("FwDisplay"),
+                ReadJsonFileAsync("FwDock"),
+                ReadJsonFileAsync("FwKb"),
+                ReadJsonFileAsync("FwMouse"),
+                ReadJsonFileAsync("Device"),
+                ReadJsonFileAsync("DeviceData"),
+                ReadJsonFileAsync("Report"),
+                ReadJsonFileAsync("DeviceConfig"),
+                ReadJsonFileAsync("DeviceConfig2"),
+                ReadJsonFileAsync("DeviceConfig3"),
+                ReadJsonFileAsync("ConfigLess"),
+                ReadJsonFileAsync("DeviceDataDisplay"),
+                ReadJsonFileAsync("Test")
+            }; 
 
-            string jsonFwDisplay = @"{""sid"":""1728273741"",""req"":[{""tid"":1,""active"":""fw"",""devicetype"":""DISPLAY"",""options"":{}}]}";
-            string jsonfwdock = @"{""sid"":""1728273741"",""req"":[{""tid"":1,""active"":""fw"",""devicetype"":""DOCK"",""options"":{}}]}";
-            string jsonfwkb = @"{""sid"":""1728273741"",""req"":[{""tid"":1,""active"":""fw"",""devicetype"":""Keyboard"",""options"":{}}]}";
-            string jsonfwmouse = @"{""sid"":""1728273741"",""req"":[{""tid"":1,""active"":""fw"",""devicetype"":""MOUSE"",""options"":{}}]}";
-
-            string jsonDevice = @"{""sid"":""1728380239"",""req"":[{""tid"":1,""active"":""get"",""devicetype"":""display"",""command"":""ConnectedDevices"",""options"":{}}]}";
-            string jsonDeviceData = @"{""sid"":""1728380255"",""req"":[{""tid"":1,""active"":""get"",""devicetype"":""APP"",""command"":""DeviceData"",""options"":{}}]}";
-
-
-            string jsonReport = @"{""sid"":""1728647205"",""req"":[{""tid"":1,""active"":""get"",""devicetype"":""APP"",""command"":""DiagnosticsReport"",""value"":""C:\\temp"",""options"":{}}]}";
-
-            string deviceconfig = "{\r\n  \"Index\": \"1\",\r\n  \"DeviceType\": \"Display\",\r\n  \"Model\": \"DELLC2722DE\",\r\n  \"SerialNumber\": \"808596812\",\r\n  \"ServiceTag\": \"CN073K0\",\r\n  \"Manufacturer\": \"Dell\",\r\n  \"ManufacturingYear\": \"2021\",\r\n  \"ManufacturingWeek\": \"ISO week 3\",\r\n  \"FirmwareVersion\": \"M3T112\",\r\n  \"MonitorActiveHour\": \"713 hours\",\r\n  \"DisplayTechnologyType\": \"LCD (active matrix)\",\r\n  \"ScreenSize\": \"600 x 340 mm (27.15 in)\",\r\n  \"OptimalResolution\": \"2560 x 1440 at 60.00Hz\",\r\n  \"Resolution\": \"1920 x 1200 at 120.00Hz\",\r\n  \"ActiveInputSource\": \"USB-C\",\r\n  \"ColorPreset\": \"Standard/Native\",\r\n  \"ScreenOrientation\": \"Landscape\",\r\n  \"BrightnessLevel\": \"90%\",\r\n  \"ContrastLevel\": \"90%\",\r\n  \"LuminanceLevel\": \"N/A\",\r\n  \"AutoBrightness\": \"off\",\r\n  \"AutoBrightnessRangeLevel\": \"N/A\",\r\n  \"AutoColorTemp\": \"off\",\r\n  \"PrimaryMonitorForSync\": \"off\",\r\n  \"AspectRatio\": \"16:9\",\r\n  \"USB_CPrioritization\": \"NOT SUPPORT\",\r\n  \"ColorManagement\": \"N/A\",\r\n  \"SpeakerMicrophone\": \"N/A\",\r\n  \"SpeakerVolume\": \"24\",\r\n  \"MicrophoneControl\": \"N/A\",\r\n  \"Uniformity\": \"N/A\",\r\n  \"PowerNap\": \"Off\",\r\n  \"OSD_language\": \"English\",\r\n  \"PID\": \"DEL421F\"\r\n}";
-            // string deviceConfig = await ReadJsonFileAsync("DeviceConfig");
-            string jsonDeviceConfig2 = "{\"sid\":\"1728380251\",\"req\":[{\"tid\":1,\"active\":\"set\",\"devicetype\":\"display\",\"command\":\"DeviceConfiguration\",\"value\":" + deviceconfig + ",\"options\":{}}]}";
-
-            string configless = "{\"Index\": \"1\",\"DeviceType\": \"Display\",\"BrightnessLevel\": \"90%\",\"ContrastLevel\": \"90%\"}";
-            string jsondeviceconfig3 = "{\"sid\":\"1728380278\",\"req\":[{\"tid\":1,\"active\":\"set\",\"devicetype\":\"display\",\"command\":\"DeviceConfiguration\",\"value\":" + configless + ",\"options\":{}}]}";
-
-            string test = @"{""sid"":""1729840262"",""req"":[{""tid"":1,""active"":""get"",""devicetype"":""APP"",""command"":""DiagnosticsReport"",""value"":""c:\temp"",""options"":{""index"":""1"",""servicetag"":""aaaaa"",""devicemodel"":""dell ea"",""uod"":true,""forcewithnotice"":true,""updatesilent"":true,""minversion"":""1.0.0.5""}},{""tid"":2,""active"":""get"",""devicetype"":""DISPLAY"",""command"":""ConnectedDevices"",""options"":{}}]}";
-
-            string jsondevicedatadisplay = @"{""sid"":""1728380255"",""req"":[{""tid"":1,""active"":""get"",""devicetype"":""display"",""command"":""DeviceData"",""options"":{}}]}";
+            string[] loadResult = await Task.WhenAll(loadJsonTasks);
+            string jsonActHours = loadResult[0].PlainJsonString();
+            string jsonGetDisplayMulti = loadResult[1].PlainJsonString();
+            string jsonFwDisplay = loadResult[2].PlainJsonString();
+            string jsonFwDock =    loadResult[3].PlainJsonString();
+            string jsonFwKb = loadResult[4].PlainJsonString();
+            string jsonFwMouse = loadResult[5].PlainJsonString();
+            string jsonDevice =    loadResult[6].PlainJsonString();
+            string jsonDeviceData = loadResult[7].PlainJsonString();
+            string jsonReport = loadResult[8].PlainJsonString();
+            string deviceConfig = loadResult[9].PlainJsonString();
+            string jsonDeviceConfig2 = loadResult[10].PlainJsonString();
+            string jsonDeviceConfig3 = loadResult[11].PlainJsonString();
+            string configLess = loadResult[12].PlainJsonString();
+            string jsonDeviceDataDisplay = loadResult[13].PlainJsonString();
+            string test = loadResult[14].PlainJsonString();
 
             RemoteRequestArgs cmaRequest = new RemoteRequestArgs();
 
@@ -203,19 +224,19 @@ namespace DDPM.CMA.Tester
                         break;
 
                     case 6:
-                        cmaRequest.remote_request = jsongetdisplaymulti;
+                        cmaRequest.remote_request = jsonGetDisplayMulti;
                         break;
 
                     case 7:
-                        cmaRequest.remote_request = jsonfwdock;
+                        cmaRequest.remote_request = jsonFwDock;
                         break;
 
                     case 8:
-                        cmaRequest.remote_request = jsonfwkb;
+                        cmaRequest.remote_request = jsonFwKb;
                         break;
 
                     case 9:
-                        cmaRequest.remote_request = jsonfwmouse;
+                        cmaRequest.remote_request = jsonFwMouse;
                         break;
 
                     case 21:
@@ -223,11 +244,11 @@ namespace DDPM.CMA.Tester
                         break;
 
                     case 22:
-                        cmaRequest.remote_request = jsondeviceconfig3;
+                        cmaRequest.remote_request = jsonDeviceConfig3;
                         break;
 
                     case 23:
-                        cmaRequest.remote_request = jsondevicedatadisplay;
+                        cmaRequest.remote_request = jsonDeviceDataDisplay;
                         break;
 
                     default:
