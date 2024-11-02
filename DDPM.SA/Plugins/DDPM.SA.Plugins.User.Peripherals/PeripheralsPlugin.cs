8df6e742921e99739e6ec881d367f223e2f7e685
brucechuang <brucechuang@wits.com>
2024-11-02 17:39:07 +0800

    [SA] 1.Fix Dock FirmwareVersion

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.Peripherals/PeripheralsPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.Peripherals/PeripheralsPlugin.cs
@@ -1420,11 +1420,11 @@ namespace DDPM.SA.Plugins.PeripheralsPlugin
                                                 {
                                                     info.ModelNumber = $"{dockData.MarketingName}_{dockData.PowerSupplyWattage}W";
                                                 }
-                                                if (string.IsNullOrEmpty(dockData.ServiceTag))
+                                                if (!string.IsNullOrEmpty(dockData.ServiceTag))
                                                 {
                                                     info.DockServiceTag = dockData.ServiceTag;
                                                 }
-                                                if (string.IsNullOrEmpty(dockData.PackageFirmwareVersion.ToString()))
+                                                if (!string.IsNullOrEmpty(dockData.PackageFirmwareVersion.ToString()))
                                                 {
                                                     info.FirmwareVersion = dockData.PackageFirmwareVersion.ToString();
                                                     info.DockPackageFwVersion = dockData.PackageFirmwareVersion.ToString();
