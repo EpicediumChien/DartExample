bef5fdf920f148e358e9e675ebb63beaca48ce00
Chewlin_zou <Chewlin_zou@wistron.com>
2024-11-15 09:44:10 +0800

    UI unit test updated 1114

---
+++ DDPM.UI/UnitTest/DDPM.UI.Plugin.Common.Tests/OpenRunModalDialogTests.cs
@@ -0,0 +1,38 @@
+﻿using DDPM.UI.Common.UserControls;
+using NGA.UnitTest.PrivateObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDPM.UI.Plugin.Common.Tests
+{
+    [Apartment(ApartmentState.STA)]
+    public class OpenRunModalDialogTests
+    {
+        private OpenRunModalDialog? openRunModalDialog;
+
+        [SetUp]
+        public void Setup()
+        {
+            openRunModalDialog = new OpenRunModalDialog(1.0, 2.0, new List<string>(), "Parameter\\p");
+        }
+
+        [Test]
+        public void TestConstructor_OpenRunModalDialog()
+        {
+            // Assert
+            Assert.That(openRunModalDialog, Is.Not.Null);
+            Assert.That(openRunModalDialog.Width, Is.EqualTo(1.0));
+            Assert.That(openRunModalDialog.Height, Is.EqualTo(2.0));
+        }
+
+        [Test]
+        public void TestParameter()
+        {
+            Assert.That(openRunModalDialog.Parameter, Is.EqualTo("Parameter\\p"));
+        }
+    }
+}
---
+++ DDPM.UI/UnitTest/DDPM.UI.Plugin.Common.Tests/UnpairModalDialogTests.cs
@@ -0,0 +1,44 @@
+﻿using DDPM.UI.Common;
+using DDPM.UI.Common.UserControls;
+using NGA.UnitTest.PrivateObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDPM.UI.Plugin.Common.Tests
+{
+    [Apartment(ApartmentState.STA)]
+    public class UnpairModalDialogTests
+    {
+        private UnpairModalDialog? unpairModalDialog;
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestConstructor_UnpairModalDialog()
+        {
+            unpairModalDialog = new UnpairModalDialog(eDeviceCategory.Mouse);
+            // Assert
+            Assert.That(unpairModalDialog, Is.Not.Null);
+
+            unpairModalDialog = new UnpairModalDialog(eDeviceCategory.KB);
+            // Assert
+            Assert.That(unpairModalDialog, Is.Not.Null);
+
+            unpairModalDialog = new UnpairModalDialog(eDeviceCategory.Pen);
+            // Assert
+            Assert.That(unpairModalDialog, Is.Not.Null);
+
+            unpairModalDialog = new UnpairModalDialog(eDeviceCategory.Headset);
+            // Assert
+            Assert.That(unpairModalDialog, Is.Not.Null);
+            Assert.That(unpairModalDialog.Height, Is.EqualTo(360));
+        }
+    }
+}
