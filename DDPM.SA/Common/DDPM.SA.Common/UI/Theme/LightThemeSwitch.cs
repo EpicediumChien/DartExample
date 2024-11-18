971400a3d48b292fd2e72615d21e685ea6ee941f
Eric Chien <EricChien@wits.com>
2024-11-18 18:57:05 +0800

    Update CapsLock Icon and OSD text color binding

--- DDPM.SA/Common/DDPM.SA.Common/UI/Theme/LightThemeSwitch.cs
+++ DDPM.SA/Common/DDPM.SA.Common/UI/Theme/LightThemeSwitch.cs
@@ -12,6 +12,7 @@ namespace DDPM.SA.Common.UI
         public static void SwitchToLightMode()
         {
             UpdateFreezable<SolidColorBrush>("Default_SA_OSD_Border_BkColor", (ref SolidColorBrush brush) => brush.Color = (Color)ColorConverter.ConvertFromString("#E6FFFFFF"));
+            UpdateFreezable<SolidColorBrush>("Default_SA_OSD_PathColor", (ref SolidColorBrush brush) => brush.Color = (Color)ColorConverter.ConvertFromString("#343434"));
         }
     }
 }
