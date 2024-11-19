77dada659ec234ba739cc06215e5c2d07e2e2c97
Rex_Cai@wistron.com <Rex_Cai@wistron.com>
2024-11-19 15:28:19 +0800

    [SA] Add CLI Webcam PRESENCEDETECTION support checking

--- DDPM.SA/Plugins/CLI.Plugins.Peripherals/CLIPeripheralsPlugins.cs
+++ DDPM.SA/Plugins/CLI.Plugins.Peripherals/CLIPeripheralsPlugins.cs
@@ -1505,26 +1505,37 @@ namespace DDPM.CLI.Plugins.Peripherals
                         x.Value = "";
                         if (x.Result == "")
                         {
-                            var result = RunAsyncTimeout(_devMgr.SetIsProximitySensorEnable(x.Guid, bl)).Result;
-                            if (result == "0")
+                            var di = _deviceinfo.Where(_ => _.ID.ToString() == x.Guid && _.LogicalDeviceType.ToUpper().Contains(_commandLineInput.PluginsType)).FirstOrDefault();
+                            if (di.IsESISupported)
                             {
-                                x.Result = "PASS";
-                                retcode_ = _devMgr.GetIsProximitySensorEnable(x.Guid).Result;
-                                x.Value = (retcode_) ? "ON" : "OFF";
-                                x.Value += "," + (data.LockSettings.Lock_Webcam_PresenceDetection ? "LOCK" : "UNLOCK");
-                                x.Message = "N/A";
-                            }
-                            else if (result == "1")
-                            {
-                                x.Result = "FAIL";
-                                x.Message = "Timeout";
+                                var result = RunAsyncTimeout(_devMgr.SetIsProximitySensorEnable(x.Guid, bl)).Result;
+                                if (result == "0")
+                                {
+                                    x.Result = "PASS";
+                                    retcode_ = _devMgr.GetIsProximitySensorEnable(x.Guid).Result;
+                                    x.Value = (retcode_) ? "ON" : "OFF";
+                                    x.Value += "," + (data.LockSettings.Lock_Webcam_PresenceDetection ? "LOCK" : "UNLOCK");
+                                    x.Message = "N/A";
+                                }
+                                else if (result == "1")
+                                {
+                                    x.Result = "FAIL";
+                                    x.Message = "Timeout";
+                                }
+                                else
+                                {
+                                    x.Result = "FAIL";
+                                    x.Message = result;
+                                }
+                                retcode = (result == "0") ? true : false;
                             }
                             else
                             {
+                                x.Value = "N/A";
                                 x.Result = "FAIL";
-                                x.Message = result;
+                                x.Message = "Webcam not support PresenceDetection";
+                                retcode = false;
                             }
-                            retcode = (result == "0") ? true : false;
                         }
                     });
                     return (retcode) ? (int)CLI_ExitCode.success : (int)CLI_ExitCode.functional_error;
