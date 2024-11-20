4699b73d41e7002d9a0457a0b4b7447db9df6070
Eric Chien <EricChien@wits.com>
2024-11-20 17:34:17 +0800

    Null handling for appModeTelementryData

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
@@ -529,22 +529,18 @@ namespace DDPM.SA.Plugins.User.DeviceManager
         {
             if (e.PropertyName == nameof(UXSystemParameters.Instance.OSTheme))
             {
-                try
-                {
-                    OSThemeEnum oSTheme = UXSystemParameters.Instance.OSTheme;
-                    if (previousOsTheme == oSTheme)
-                        return;
-                    var applicationSettings_Function = new ApplicationSettings_Function();
-                    string appModeTelementryData = loadResourceDictionary(oSTheme);
-                    Debug.WriteLine($"AppModeTelemetry=> {appModeTelementryData}");
-                    writelog("[DeviceMangerPlugin] [Telementry] Send Telementry for AppMode...");
-                    Task.Run(() => applicationSettings_Function.Send_AppMode_Telementry(_TelementryScheduler, _AllInfoMonitors, appModeTelementryData)).ConfigureAwait(false);
-                    /* if (rt) writelog("[DeviceMangerPlugin] [Telementry] Send Telementry for AppMode Success ...");
-                     else writelog("[DeviceMangerPlugin] [Telementry] Send Telementry for AppMode Fail ...");*/
-                }
-                catch (Exception ex) { 
-                    writelog($"Exception msg: {ex.Message} Exception StackTrace: {ex.StackTrace}", nameof(UXSystemParametersChanged), "DeviceManagerPlugin.cs");
-                }
+                OSThemeEnum oSTheme = UXSystemParameters.Instance.OSTheme;
+                if (previousOsTheme == oSTheme)
+                    return;
+                var applicationSettings_Function = new ApplicationSettings_Function();
+                string appModeTelementryData = loadResourceDictionary(oSTheme);
+                if(string.IsNullOrEmpty(appModeTelementryData))
+                    writelog($"[appModeTelementryData] cannot be null", nameof(appModeTelementryData));
+                Debug.WriteLine($"AppModeTelemetry=> {appModeTelementryData}");
+                writelog("[DeviceMangerPlugin] [Telementry] Send Telementry for AppMode...");
+                Task.Run(() => applicationSettings_Function.Send_AppMode_Telementry(_TelementryScheduler, _AllInfoMonitors, appModeTelementryData)).ConfigureAwait(false);
+                /* if (rt) writelog("[DeviceMangerPlugin] [Telementry] Send Telementry for AppMode Success ...");
+                 else writelog("[DeviceMangerPlugin] [Telementry] Send Telementry for AppMode Fail ...");*/
             }
         }
 
