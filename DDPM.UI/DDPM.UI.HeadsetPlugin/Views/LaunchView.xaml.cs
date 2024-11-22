0a7ec4e34ebc2733c469dd22afcb7889a95c742a
Wayn_chen <Wayn_chen@wistron.com>
2024-11-22 17:26:20 +0800

    Update

--- DDPM.UI/DDPM.UI.HeadsetPlugin/Views/LaunchView.xaml.cs
+++ DDPM.UI/DDPM.UI.HeadsetPlugin/Views/LaunchView.xaml.cs
@@ -351,6 +351,8 @@ namespace DDPM.UI.Plugin.HeadsetPlugin
             }
             else if (_vm!.ConnectionType == "Bluetooth")//I can't get Headset connection HostName, FW issue?
             {
+                int pp = DdpmCommonHelper.DeviceManagerSA.GetTotalNumberOfPairedHostNameAsync(_vm.CurrentDeviceInfo.ID.ToString()).Result;
+                //_deviceManager.GetFirmwareVersionAsync(CurrentDeviceID.ToString()).Result;
                 string hostName = Dns.GetHostName();
                 //var hostIndex = _vm!.VisiblePairedHostName1.ToUpper() == "VISIBLE" ? 1 : (_vm.VisiblePairedHostName2.ToUpper() == "VISIBLE" ? 2 : 3);
                 txt1.Style = ConnectionStyle1;
