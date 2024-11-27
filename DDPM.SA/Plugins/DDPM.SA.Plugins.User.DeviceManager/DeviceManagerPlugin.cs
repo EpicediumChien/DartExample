72f5ccd2f948097710f90ca45435096ae40e5c6c
Dean Yang(WISTRON) <dean_yang@wistron.com>
2024-11-27 16:29:53 +0800

    [SA] remove the usage with new X509Certificate2 to solved SDL issue

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
@@ -610,6 +610,9 @@ namespace DDPM.SA.Plugins.User.DeviceManager
             thread.Start();
 
             _disDevHelper = new DisplayDeviceHelper(Log);
+
+            string info = string.Empty;
+            DDPMFileSecurity.VerifyFileCertWithInboxThumbprint("D:\\DDPM\\metadata\\DDPM-Setup_v2.0.0.51.exe", out info);
         }
 
         private void HotkeyPressed(object sender, KeyPressedEventArgs e)
