c5eabcec2c9db4271490fc3b05f521695a1734fd
Eric Chien <EricChien@wits.com>
2024-11-29 12:25:10 +0800

    Fix cannot load device bug for homepage

--- DDPM.UI/DDPM.UI.DdpmHomePlugin/HomeDevicesPanel.cs
+++ DDPM.UI/DDPM.UI.DdpmHomePlugin/HomeDevicesPanel.cs
@@ -8,7 +8,7 @@ namespace DDPM.UI.Plugin.DdpmHomePlugin
 {
     //Reference:
     //https://stackoverflow.com/questions/806777/wpf-how-can-i-center-all-items-in-a-wrappanel
-    internal class HomeDevicesPanel : Panel
+    internal class HomeDevicesPanel : WrapPanel
     {
         public HorizontalAlignment HorizontalContentAlignment
         {
