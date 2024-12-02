d913769324e9c5704aacb0de30680eebd7e7163e
Elie_Liao <Elie_Liao@wistron.com>
2024-12-02 11:03:02 +0000

    Revert "Merged PR 1432: QAM lanuch DDPM process update
    QAM lanuch DDPM process update"

--- DDPM.SA/Common/DDPM.QAM/QAMPage.xaml.cs
+++ DDPM.SA/Common/DDPM.QAM/QAMPage.xaml.cs
@@ -1,13 +1,11 @@
 ﻿using DDPM.SA.Common;
 using Dell.Client.Framework.Common;
 using Dell.Client.Framework.UX.WPF.Controls;
-using Microsoft.VisualBasic.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -27,8 +25,6 @@ namespace DDPM.QAM
     public partial class QAMPage : Window
     {
         CameraSetting CameraSetting;
-
-        public event EventHandler<UpdateUINotify> UpdateUINotify;
 
         [DllImport("user32.dll", SetLastError = true)]
         [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
@@ -51,56 +47,24 @@ namespace DDPM.QAM
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private bool ShowDDPM()
+        private void ShowDDPM()
         {
             const int SW_SHOWNORMALSW_NORMAL = 1;
             string processName = "DDPM";
-            bool result = false;
 
             if (OperatingSystem.IsWindows())
             {
-                try
+                Process[] processes = Process.GetProcessesByName(processName);
+
+                if (processes.Length > 0)
                 {
-                    Process[] processes = Process.GetProcessesByName(processName);
-
-                    if (processes.Length > 0)
-                    {
-                        IntPtr mainWindowHandle = processes[0].MainWindowHandle;
-                        // 將窗口最大化
-                        _ShowWindow(mainWindowHandle, SW_SHOWNORMALSW_NORMAL);
-                        // 顯示到前景
-                        _SetForegroundWindow(mainWindowHandle);
-
-                        //info DDPM navigate to webcam preview directly
-                        DdpmCommonHelper.DeviceManagerSA!.SetIsDDPMLaunchByQAMAsync(true);
-                        DdpmCommonHelper.DeviceManagerSA!.SetIsDDPMHomepageReadyAsync(true);
-
-                        result = true;
-                    }
-                    else
-                    {
-                        string ddpmExePath = @"C:\Program Files\Dell\Dell Display and Peripheral Manager\DDPM.exe";
-                        //string debugPath = "D:\\DDPM\\DDPM.UI\\bin\\net8.0-windows10.0.19041.0\\DDPM.exe";
-
-                        result = DDPM.SA.Common.Settings.DDPMFileSecurity.StartProcessSafely(
-                            null,
-                            new ProcessStartInfo
-                            {
-                                FileName = ddpmExePath,
-                                UseShellExecute = true
-                            });
-
-                        //Info SA that new DDPM instance launched by QAM
-                        DdpmCommonHelper.DeviceManagerSA!.SetIsDDPMLaunchByQAMAsync(true);
-                    }
-                }
-                catch
-                {
-                    result = false;
+                    IntPtr mainWindowHandle = processes[0].MainWindowHandle;
+                    // 將窗口最大化
+                    _ShowWindow(mainWindowHandle, SW_SHOWNORMALSW_NORMAL);
+                    // 顯示到前景
+                    _SetForegroundWindow(mainWindowHandle);
                 }
             }
-
-            return result;
         }
 
         public QAMPage(IDeviceManagerSA deviceMangerPlugin)
@@ -128,10 +92,10 @@ namespace DDPM.QAM
 
         private void Close_Click(object sender, MouseButtonEventArgs e)
         {
-            CloseMyself(true);
+            CloseMyself();
         }
 
-        private void CloseMyself(bool closedByUser = false)
+        private void CloseMyself()
         {
             if (CameraSetting != null)
             {
@@ -139,16 +103,11 @@ namespace DDPM.QAM
                 CameraSetting = null;
             }
 
-            if (closedByUser)
-                this.Close();
-            else
+            Dispatcher.Invoke(() =>
             {
-                Dispatcher.Invoke(() =>
-                {
-                    // Access UI elements or objects owned by a different thread
-                    this.Close();
-                });
-            }
+                // Access UI elements or objects owned by a different thread
+                this.Close();
+            });
         }
 
         private void CameraSetting_Click(object sender, MouseButtonEventArgs e)
@@ -161,57 +120,18 @@ namespace DDPM.QAM
             else
             {
                 CameraSetting = new CameraSetting();
-
                 CameraSetting.Left = this.Left + this.Width;
                 CameraSetting.Top = this.Top;
                 CameraSetting.Width = 288;
                 CameraSetting.Height = 128;
                 CameraSetting.Show();
             }
-
-            QAMPageViewModel vm = DataContext as QAMPageViewModel;
-
-            if (vm != null && vm.CurrentDeviceInfo != null)
-                vm.IsCameraSettingSelected = true;
         }
 
         private void CallDDPM_Click(object sender, MouseButtonEventArgs e)
         {
-            //ShowDDPM();
-            //SendMessageToDDPM(10);
-
-            if (ShowDDPM())
-            {
-                //Info SA that DDPM launched by QAM
-                //DdpmCommonHelper.DeviceManagerSA!.SetIsDDPMLaunchByQAMAsync(true);
-
-                Close_Click(this, null);
-            }
-            else
-                DdpmCommonHelper.DeviceManagerSA!.SetIsDDPMLaunchByQAMAsync(false);
-        }
-
-        private void SendMessageToDDPM(int timeout)
-        {
-            int i = 0;
-
-            while (true)
-            {
-                Thread.Sleep(1000);
-                ++i;
-
-                //wait for homepage is available
-                if (2000 == DdpmCommonHelper.DeviceManagerSA!.GetCurrentPollingRate().Result)
-                {
-                    Thread.Sleep(2000);
-                    OnUpdateUINotify($"QAMEvent_StartPreview[{i}]");
-
-                    break;
-                }
-
-                if (i >= 10)
-                    break;
-            }
+            ShowDDPM();
+            Close_Click(this, null);
         }
 
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
@@ -225,14 +145,6 @@ namespace DDPM.QAM
                     CameraSetting.Top = this.Top;
                 }
             }
-        }
-
-        public void OnUpdateUINotify(string msg)
-        {
-            UpdateUINotify e = new UpdateUINotify();
-            e.UI_Field_Name = msg;
-
-            UpdateUINotify?.Invoke(this, e);
         }
     }
 }
