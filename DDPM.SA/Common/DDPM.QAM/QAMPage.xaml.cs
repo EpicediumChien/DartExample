83ce8ece94937e7d7424186f83dd38673d0f2e83
Derek_Du <derek_du@wistron.com>
2024-12-02 18:25:01 +0800

    Update DDPM.exe path

--- DDPM.SA/Common/DDPM.QAM/QAMPage.xaml.cs
+++ DDPM.SA/Common/DDPM.QAM/QAMPage.xaml.cs
@@ -79,14 +79,14 @@ namespace DDPM.QAM
                     }
                     else
                     {
-                        //string ddpmExePath = @"C:\Program Files\Dell\Dell Display and Peripheral Manager\DDPM.exe";
-                        string debugPath = "D:\\DDPM\\DDPM.UI\\bin\\net8.0-windows10.0.19041.0\\DDPM.exe";
+                        string ddpmExePath = @"C:\Program Files\Dell\Dell Display and Peripheral Manager\DDPM.exe";
+                        //string debugPath = "D:\\DDPM\\DDPM.UI\\bin\\net8.0-windows10.0.19041.0\\DDPM.exe";
 
                         result = DDPM.SA.Common.Settings.DDPMFileSecurity.StartProcessSafely(
                             null,
                             new ProcessStartInfo
                             {
-                                FileName = debugPath,
+                                FileName = ddpmExePath,
                                 UseShellExecute = true
                             });
 
