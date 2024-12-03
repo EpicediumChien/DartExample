af5cd8bb547e13fee470b482106d86b150ce11a4
Elie_Liao <Elie_Liao@wistron.com>
2024-12-03 11:23:30 +0000

    Revert "Merged PR 1459: Merge QAM1202 to Dev
    Merge QAM1202 to Dev"

--- DDPM.UI/DDPM.UI.DdpmHomePlugin/ConnectionHoverView.xaml.cs
+++ DDPM.UI/DDPM.UI.DdpmHomePlugin/ConnectionHoverView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using DDPM.UI.Common;
-using UserControl = System.Windows.Controls.UserControl;
+﻿using UserControl = System.Windows.Controls.UserControl;
 
 namespace DDPM.UI.Plugin.DdpmHomePlugin
 {
@@ -12,19 +11,6 @@ namespace DDPM.UI.Plugin.DdpmHomePlugin
         public ConnectionHoverView()
         {
             InitializeComponent();
-        }
-
-        private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
-        {
-            //Derek 1129 check if DDPM is launched by QAM, then info SA that homepage is ready
-            //DdpmCommonHelper.DDPMMesssageBox("UserControl_Loaded", "DDPMMesssageBox");
-            if (DdpmCommonHelper.DeviceManagerSA!.GetIsDDPMLaunchByQAM().Result == true)
-            {
-                DdpmCommonHelper.WriteUILog($"GetIsDDPMLaunchByQAM = true from ConnectionHoverView::UserControl_Loaded");
-
-                DdpmCommonHelper.DeviceManagerSA!.SetIsDDPMHomepageReadyAsync(true);
-                DdpmCommonHelper.DeviceManagerSA!.SetIsDDPMLaunchByQAMAsync(false);
-            }
         }
     }
 }
