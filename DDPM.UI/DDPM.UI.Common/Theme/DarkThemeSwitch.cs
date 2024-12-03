28acd54c5584974926d81c663858ae3611873130
Eric Chien <EricChien@wits.com>
2024-12-03 14:29:10 +0800

    Fix consent page

--- DDPM.UI/DDPM.UI.Common/Theme/DarkThemeSwitch.cs
+++ DDPM.UI/DDPM.UI.Common/Theme/DarkThemeSwitch.cs
@@ -149,7 +149,10 @@ namespace DDPM.UI.Common
                 #region Main Window Background
                 SplashPath = "Resources/Images/splash{0}-round_Dark.png";
                 UpdateFreezable("mainWindowBkColor", (ref SolidColorBrush brush) => brush.Color = (Color)ColorConverter.ConvertFromString("#F20D121A"));
-                UpdateFreezable("bkImage", (ref ImageBrush brush) => brush.ImageSource = GetImageSourceFromCommonResource("Resources/Images/Background.png", "DDPM"));
+                UpdateFreezable("bkImage", (ref ImageBrush brush) => {
+                    brush.Stretch = Stretch.Fill;
+                    brush.ImageSource = GetImageSourceFromCommonResource("Resources/Images/Background.png", "DDPM");
+                    });
                 UpdateFreezable("SpinnerWaitTxtColor", (ref SolidColorBrush brush) => brush.Color = (Color)ColorConverter.ConvertFromString("#F5F6F7"));
                 UpdateFreezable("SpinnerBkColor", (ref SolidColorBrush brush) => brush.Color = (Color)ColorConverter.ConvertFromString("#F20D121A"));
                 #endregion
@@ -321,6 +324,10 @@ namespace DDPM.UI.Common
                 UpdateFreezable("ConnHover_ActiveTextColor", (ref SolidColorBrush brush) => brush.Color = (Color)ColorConverter.ConvertFromString("#FFFFFF"));
                 UpdateFreezable("ConnHover_DeactiveTextColor", (ref SolidColorBrush brush) => brush.Color = (Color)ColorConverter.ConvertFromString("#839DB4"));//839DB4
                 #endregion Connection Hover UI
+
+                #region Consent page
+                UpdateFreezable("ConsentPage_Background", (ref SolidColorBrush brush) => brush.Color = (Color)ColorConverter.ConvertFromString("#13172A"));
+                #endregion
 
                 //#region HomeDevice LineArt image [Robert_Lin, 2024-11-26]
                 UpdateBitmapImage("MonitorImage_LineArt", new Uri($"pack://application:,,,/DDPM.UI.Resources;component/Resources/Monitors/Lineart.png", UriKind.RelativeOrAbsolute));
