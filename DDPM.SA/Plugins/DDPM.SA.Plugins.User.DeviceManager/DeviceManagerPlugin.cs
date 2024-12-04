65cd7f3b927d210d9617bcb97179339b2b97be36
gavin_liu <gavin_liu@wistron.com>
2024-12-04 16:31:19 +0800

    1.PIMS-326111,Lanuch Hotkey: Recent layout, we move apps or folder, windows can not snap to the zone when drag into it

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
@@ -12870,31 +12870,58 @@ namespace DDPM.SA.Plugins.User.DeviceManager
             //Read the EAMonitorSettings
             EAMonitorSettings eaSettings = ReadEAMonitorSettings(monitorInfo).Result;
             //Change selected layout to the latest item of RecentList
-            int idxRecent = 0;
-            if (eaSettings.RecentList == null)
+            if (eaSettings != null)
             {
-                writelog("@ Toggle_EzRecentSetting(), EA RecentList is null");
-                return;
+                SplitJson[] recentList = eaSettings.RecentList;
+
+                if (recentList == null)
+                {
+                    writelog("@ Toggle_EzRecentSetting(), EA RecentList is null");
+                    return;
+                }
+                if (recentList.Length == 0)
+                {
+                    writelog("@ Toggle_EzRecentSetting(), EA RecentList is empty");
+                    return;
+                }
+                else
+                {
+                    List<SplitJson> splitJsonsList = recentList.ToList();
+                    List<SplitJson> splitJsonsTmp = new List<SplitJson>();
+                    splitJsonsTmp.AddRange(splitJsonsList);
+                    SplitJson LastSplitJson = splitJsonsTmp.ElementAt(splitJsonsList.Count - 1);
+                    splitJsonsTmp.RemoveAt(splitJsonsList.Count - 1);
+                    splitJsonsTmp.Insert(0, LastSplitJson);
+                    splitJsonsList.Clear();
+                    splitJsonsList.AddRange(splitJsonsTmp);
+                    //save recentlist
+                    eaSettings.RecentList = splitJsonsList.ToArray();
+                    eaSettings.SelectedSplit = splitJsonsList.ElementAt(0);
+                    bool result = WriteEAMonitorSettings(monitorInfo, eaSettings).Result;
+                    if (result)
+                    {
+                        //Change selected layout to the latest item of RecentList
+                        bool changed = NotifyEASelectedLayoutChanged(monitorInfo, splitJsonsList.ElementAt(0)).Result;
+                        //notice UI
+                        EAArgs eAArgs = new EAArgs();
+                        eAArgs.Message = @"updateRecentSelected";
+                        EASettingsChanged(this, eAArgs);
+                        writelog($"Toggle_EzRecentSetting(),monitor:{monitorInfo.AliasDeviceName}={monitorInfo.edid.ServiceTag}, EA RecentList.Count={eaSettings.RecentList.Length}, toggle to [{splitJsonsList.ElementAt(0).CellCount},{splitJsonsList.ElementAt(0).SplitKey}],save eaSettings.RecentList success");
+                    }
+                    else
+                    {
+                        writelog($"Toggle_EzRecentSetting(),monitor:{monitorInfo.AliasDeviceName}={monitorInfo.edid.ServiceTag}, EA RecentList.Count={eaSettings.RecentList.Length}, toggle to [{splitJsonsList.ElementAt(0).CellCount},{splitJsonsList.ElementAt(0).SplitKey}],save eaSettings.RecentList fail, do nothing");
+                    }
+                }
             }
-            if (eaSettings.RecentList.Length == 0)
-            {
-                writelog("@ Toggle_EzRecentSetting(), EA RecentList is empty");
-                return;
-            }
-            else
-            {
-                //Should be always EAEMConstants.MaxRecentItems(=5)-1 = 4
-                writelog($"@ Toggle_EzRecentSetting(), EA RecentList.Count={eaSettings.RecentList.Length}");
-            }
-            idxRecent = eaSettings.RecentList.Length - 1;
 
             //Force await to avoid reenter this method (it will update to MonitorSettings file)
-            bool isOKSetSelected = SetEASelectedLayout(monitorInfo, eaSettings.RecentList[idxRecent]).Result;
+            //bool isOKSetSelected = SetEASelectedLayout(monitorInfo, eaSettings.RecentList[idxRecent]).Result;
 
             //TO DO: invoke an event to UI to reload settings
             // TO be implement in EASettingsChanged event
 
-            writelog($"@ Toggle_EzRecentSetting(), result is {isOKSetSelected}");
+            //writelog($"@ Toggle_EzRecentSetting(), result is {isOKSetSelected}");
         }
 
         private bool IsHotkeyFuncLock(HotkeyType type)
