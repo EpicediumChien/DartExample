608d0ce095bb7f9a507cdacb3940797a4f27ba7c
Hess Cheng <HessCheng@wits.com>
2024-12-04 21:26:07 +0800

    [SA/UI] fix PIMS-328347 Pan Tilt arrows appear for Webcam even when no Zoom is done.

--- DDPM.SA/Common/DDPM.SA.Common/Interfaces/IDeviceManagerSA.cs
+++ DDPM.SA/Common/DDPM.SA.Common/Interfaces/IDeviceManagerSA.cs
@@ -809,6 +809,9 @@ namespace DDPM.SA.Common
         Task<bool> GetIsAutoFramingOn(string Guid);
         Task<string> GetSupportedResolutions(string Guid);
         Task<string> GetSelectedResolution(string Guid);
+        Task<int> GetZoom(string Guid);
+        Task<int> GetFocus(string Guid);
+        Task<bool> GetIsFocusOn(string Guid);
 
         Task SetIsMicEnumerationOn(string Guid, bool newValue);
 
