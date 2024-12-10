b2899f7f50d83bf49b19e95b53c578184c5c4dc3
Elie_Liao <Elie_Liao@wistron.com>
2024-12-10 11:23:33 +0000

    Revert "Merged PR 1569: fix same model same service tag pop up modalDialog issue and fix [PIMS-314182] [DDPM Win 2.0][R14 ]The option of _Automatically apply the settings when the same model is detected_ has no function while same model is detected
    [PIMS-314182] [DDPM Win 2.0][R14 ]The option of _Automatically apply the settings when the same model is detected_ has no function while same model is detected
    
    fix same model same service tag pop up modalDialog issue"

--- DDPM.UI/DDPM.UI.DdpmHomePlugin/Views/DdpmHomePage.xaml.cs
+++ DDPM.UI/DDPM.UI.DdpmHomePlugin/Views/DdpmHomePage.xaml.cs
@@ -69,76 +69,42 @@ namespace DDPM.UI.Plugin.DdpmHomePlugin
 
                     DDPMImpExpSettings ImpExpSettings = new DDPMImpExpSettings();
                     ImpExpSettings = DdpmCommonHelper.DeviceManagerSA.ReadImportSettingsFile(exportpath).Result;
-                    string ImpExpServiceTag = ImpExpSettings.MonitorSettings.ServiceTag;
-                    //Elsa add to fix [PIMS-314182]
-                    string setpath = localAppDataPath + "\\Dell Display and Peripheral Manager\\Display";
-                    string settingPath = setpath + "\\" + model + ".json";
-                    List<DDPMMonitorSettings> monitorSettings = DdpmCommonHelper.DeviceManagerSA.ReloadMonitorSettings(model).Result;
-                    DDPMMonitorSettings monitorSetting = null;
-                    if (monitorSettings != null)
+
+                    if (ImpExpSettings != null)
                     {
-                        if (monitorSettings.Count != 0)
+                        if (ImpExpSettings.MonitorSettings != null)
                         {
-                            foreach (DDPMMonitorSettings settings in monitorSettings)
-                            {
-                                if (settings.ServiceTag == ImpExpServiceTag)
-                                {
-                                    monitorSetting = settings;
-                                }
-                            }
-                        }
-                    }
-                    if (ImpExpSettings != null && monitorSetting != null)
-                    {
-                        if (monitorSetting.ImpExpSettings != null)
-                        {
-                            if (monitorSetting.ImpExpSettings.SameModel)//Elsa modify check ImpExpSettings.ImpExpSettings.SameModel to monitorSetting.ImpExpSettings.SameModel to fix [PIMS-314182]
+                            if (ImpExpSettings.MonitorSettings.ImpExpSettings.SameModel)
                             {
                                 DdpmCommonHelper.DeviceManagerSA.DisplayImportSettings(mo, true, exportpath).Wait();
                             }
                             else
                             {
-                                //Elsa add to fix same model same service tag pop up modalDialog issue
-                                if (ImpExpSettings.MonitorSettings.Model == mo.modelName&& ImpExpSettings.MonitorSettings.ServiceTag == mo.edid.ServiceTag)
+                                Window parentWindow = Window.GetWindow(this);
+                                double windowLeft = 0;
+                                double windowTop = 0;
+                                ImportModalDialog modalDialog = new(mo.modelName, parentWindow.ActualWidth, parentWindow.ActualHeight - 40);
+                                if (parentWindow != null)
                                 {
-                                    DdpmCommonHelper.DeviceManagerSA.DisplayImportSettings(mo, true, exportpath).Wait(); 
+                                    modalDialog.Owner = parentWindow;
+                                    windowLeft = parentWindow.Left;
+                                    windowTop = parentWindow.Top + 40;
                                 }
-                                else
+                                modalDialog.WindowStartupLocation = WindowStartupLocation.Manual;
+                                modalDialog.Left = windowLeft;
+                                modalDialog.Top = windowTop;
+                                modalDialog.ShowDialog();
+
+                                if (modalDialog.DialogResult != null && modalDialog.DialogResult == true)
                                 {
-                                    try
+                                    //For jason to do import
+                                    if (DdpmCommonHelper.DeviceManagerSA.DisplayImportSettings(mo, true, exportpath).Result)
                                     {
-                                        Window parentWindow = Window.GetWindow(this);
-                                        double windowLeft = 0;
-                                        double windowTop = 0;
-                                        ImportModalDialog modalDialog = new(mo.modelName, parentWindow.ActualWidth, parentWindow.ActualHeight - 40);
-                                        if (parentWindow != null)
+                                        //ignore next check for this model
+                                        if (modalDialog.isChecked)
                                         {
-                                            modalDialog.Owner = parentWindow;
-                                            windowLeft = parentWindow.Left;
-                                            windowTop = parentWindow.Top + 40;
+                                            DdpmCommonHelper.DeviceManagerSA.SetSameModel(mo, true);
                                         }
-                                        modalDialog.WindowStartupLocation = WindowStartupLocation.Manual;
-                                        modalDialog.Left = windowLeft;
-                                        modalDialog.Top = windowTop;
-                                        modalDialog.ShowDialog();
-
-                                        if (modalDialog.DialogResult != null && modalDialog.DialogResult == true)
-                                        {
-                                            //For jason to do import
-                                            if (DdpmCommonHelper.DeviceManagerSA.DisplayImportSettings(mo, true, exportpath).Result)
-                                            {
-                                                //ignore next check for this model
-                                                if (modalDialog.isChecked)
-                                                {
-                                                    DdpmCommonHelper.DeviceManagerSA.SetSameModel(mo, true);
-                                                }
-                                            }
-                                        }
-
-                                    }
-                                    catch (Exception e)
-                                    {
-                                        DdpmCommonHelper.WriteUILog($"catac excpetion[{e.Message}] when run ImportNotifyEventHandler");
                                     }
                                 }
                             }
