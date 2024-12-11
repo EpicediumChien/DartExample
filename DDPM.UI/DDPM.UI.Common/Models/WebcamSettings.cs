dacc774de0daa66547505f432b5bc470b21f37e9
Hess Cheng <HessCheng@wits.com>
2024-12-11 17:17:04 +0800

    [UI] fix PIMS-330852 In capture it is show 4K QHD is wrong. (4K is UHD)

--- DDPM.UI/DDPM.UI.Common/Models/WebcamSettings.cs
+++ DDPM.UI/DDPM.UI.Common/Models/WebcamSettings.cs
@@ -74,7 +74,7 @@ namespace DDPM.UI.Common
                         "1280x720" => "HD",
                         "1920x1080" => "Full HD",
                         "2560x1440" => "2K QHD",
-                        "3840x2160" => "4K QHD",
+                        "3840x2160" => "4K UHD",
                         _ => "8K UHD"
                     };
                     SupportedFPSs.Add(resName, res.Value.FPS);
