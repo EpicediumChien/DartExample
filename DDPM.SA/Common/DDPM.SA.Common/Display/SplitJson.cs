4c6dfb8e6601a8616f605719e4d638a4975520e3
Robert_Lin <Robert_Lin@wistron.com>
2024-12-12 17:02:30 +0800

    Fix bug: EA Custom layout in Recent list is not updated when the custom layout is edited by another monitor.

--- DDPM.SA/Common/DDPM.SA.Common/Display/SplitJson.cs
+++ DDPM.SA/Common/DDPM.SA.Common/Display/SplitJson.cs
@@ -95,6 +95,26 @@ namespace DDPM.SA.Common.Display
             return true;
         }
 
+        //Robert_Lin, 2024-12-12 General (Static) method to compare two Settings (List<double>) are the same.
+        /// <summary>
+        /// Compare two Settings (List of double) are equal.
+        /// A. Both are null  : return true
+        /// B. Both are non-null: Copmare with List.SequenceEqual() to compare if all elements in both list are the same values in sequence.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static bool AreSettingsEqual(List<double> x, List<double> y)
+        {
+            if ((x== null) && (y== null))
+                return true;
+            if (x == null) return false;
+            if (y == null) return false;
+            if (x.Count != y.Count) 
+                return false;
+
+            return x.SequenceEqual(y);
+        }
         #region Defaul Recent List
         /// <summary>
         /// Need to copy the DefaultSettings from DDPM.Easy.Common/SplitCtrlXX.xaml.cs
