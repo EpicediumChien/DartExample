98306ae702205e7e4b8f4a6b0e0bd06eeb084485
Elie_Liao <Elie_Liao@wistron.com>
2024-12-13 14:47:45 +0000

    Revert "Merged PR 1667: EAPlugin: Add Set/Get SeletedLayout by EAID, ready for CLI usage.
    2 methods are added:
    public Task<bool> EAPlugin.SetEASelectedLayout(MonitorInfo monitorInfo, int eaId)
    public Task<int> DeviceManagerPlugn.GetEASelectedLayout(MonitorInfo monitorInfo)"

--- DDPM.SA/Common/DDPM.EABroker/InfoWindow.xaml.cs
+++ DDPM.SA/Common/DDPM.EABroker/InfoWindow.xaml.cs
@@ -18,7 +18,6 @@ using DDPM.Win32Lib;
 using DDPM.SA.Common.Display;
 using DDPM.SA.Common;
 using VcpCore.Common;
-using DDPM.SA.Common.Settings;
 
 
 namespace DDPM.EABroker
@@ -318,79 +317,25 @@ namespace DDPM.EABroker
         #endregion Window Event Handlers
 
         #region Layouts
-        private void refreshMonitorsButton_Click(object sender, RoutedEventArgs e)
+        private void InitLayoutList()
         {
-            List<MonitorInfo>? monitors = _vm.GetMonitors();
-            if (monitors == null) return;
-            cbMonitors.ItemsSource = monitors;
-        }
-
-        private void SetSelectedLayoutButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (cbMonitors.SelectedItem == null) return;
-            MonitorInfo monitorInfo = cbMonitors.SelectedItem as MonitorInfo;
-
-            string eaIdText = tbEAId.Text;
-            if (String.IsNullOrEmpty(eaIdText)) return;
-            int eaId = 0;
-            if (int.TryParse(eaIdText, out eaId))
+            foreach (ISplitCtrl isp in ISplitCtrl.Splits_EA)
             {
-                _vm.SetEASelectedLayout(monitorInfo, eaId);
+                lbLayouts.Items.Add($"({isp.EAID}) {isp.CtrlClass}");
             }
         }
-
-        private void refreshRecentListButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (cbMonitors.SelectedItem == null) return;
-            MonitorInfo monitorInfo = cbMonitors.SelectedItem as MonitorInfo;
-            EAMonitorSettings eaSettings = _vm.ReadEAMonitorSettings(monitorInfo);
-            if (eaSettings == null) return;
-
-            SplitJson spjSelected = eaSettings.SelectedSplit;
-            StringBuilder sb = new StringBuilder();
-
-            string mark = "*";
-            foreach (SplitJson spjRecent in eaSettings.RecentList)
-            {
-                if (spjRecent.IsEquals(spjSelected))
-                {
-                    sb.Append(mark);
-                }
-                sb.Append($"[{spjRecent.EAID}]{spjRecent.CellCount}{spjRecent.SplitKey}");
-                sb.Append("   ");
-            }
-            string strOut = sb.ToString();
-            strOut = strOut.Trim();
-            txtRecentList.Text = strOut;
-        }
-
-        private void refreshCustoListButton_Click(object sender, RoutedEventArgs e)
-        {
-            SplitJson[] customList = _vm.ReadCustomList();
-            StringBuilder sb = new StringBuilder();
-
-            foreach (SplitJson spjCustom in customList)
-            {
-                sb.Append($"[{spjCustom.EAID}]{spjCustom.CellCount}{spjCustom.SplitKey}");
-                sb.Append("   ");
-            }
-            string strOut = sb.ToString();
-            strOut = strOut.Trim();
-            txtCustomList.Text = strOut;
-        }
-
         private void reloadCustomLayoutsButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
         private void sekectLayoutButton_Click(object sender, RoutedEventArgs e)
         {
-            //object selItem = lbLayouts.SelectedItem;
-            //if (selItem != null)
-            //{
-            //    System.Windows.Interop.WindowInteropHelper wndHelper = new System.Windows.Interop.WindowInteropHelper(this);
-            //    Screen scr = Screen.FromHandle(wndHelper.Handle);
-            //}
+            object selItem = lbLayouts.SelectedItem;
+            if (selItem != null)
+            {
+                System.Windows.Interop.WindowInteropHelper wndHelper = new System.Windows.Interop.WindowInteropHelper(this);
+                Screen scr = Screen.FromHandle(wndHelper.Handle);
+            }
         }
         #endregion
 
@@ -421,6 +366,5 @@ namespace DDPM.EABroker
             };
             _vm.Invoke_EditCommand(mi, eaArgs);
         }
-
     }
 }
