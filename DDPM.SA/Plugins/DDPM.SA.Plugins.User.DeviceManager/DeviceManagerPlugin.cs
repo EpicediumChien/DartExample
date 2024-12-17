aefd6de545646f1c6671502551d839f8d0a4ba89
gavin_liu <gavin_liu@wistron.com>
2024-12-17 16:33:50 +0800

    1.multi-language related updates

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
@@ -10488,7 +10488,7 @@ namespace DDPM.SA.Plugins.User.DeviceManager
                 _QAM = null;
 
                 if (_GlobalSettingParam != null && _GlobalSettingParam.GlobalSetting_WidgetSettings != null
-                    && _GlobalSettingParam.GlobalSetting_WidgetSettings.EnableQuickAccessWidget_Reminder && 
+                    && _GlobalSettingParam.GlobalSetting_WidgetSettings.EnableQuickAccessWidget_Reminder &&
                     isOpenOSDWhenQAMClosed)
                 {
                     ShowOSD(Screen.PrimaryScreen.DeviceName, OSDType.QAM);
@@ -12632,7 +12632,8 @@ namespace DDPM.SA.Plugins.User.DeviceManager
                 }
 
                 //overwrite conflict key
-                HotkeyInfo conflictKeyinfo = curHotkey.HotkeyInfo.SingleOrDefault(x => KeysTostr(x.Hotkey).Equals(KeysTostr(hotkeys)));
+                Debug.WriteLine($"overwrite conflict key: {KeysTostr(hotkeys)}");
+                HotkeyInfo conflictKeyinfo = curHotkey.HotkeyInfo.SingleOrDefault(x => !string.IsNullOrEmpty(KeysTostr(hotkeys)) && KeysTostr(x.Hotkey).Equals(KeysTostr(hotkeys)));
                 if (conflictKeyinfo != null)
                 {
                     conflictKeyinfo.Hotkey = new List<VirtualKey>();
