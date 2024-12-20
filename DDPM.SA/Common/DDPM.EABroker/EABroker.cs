fe790e4bdb1848f860b70b083ca0b5648ed079a8
Sheng-Chieh Chou <jc800703@hotmail.com>
2024-12-20 11:42:13 +0800

    [SA] Remove this useless assignment to local variable 'newSpanEnabled'.

--- DDPM.SA/Common/DDPM.EABroker/EABroker.cs
+++ DDPM.SA/Common/DDPM.EABroker/EABroker.cs
@@ -244,13 +244,12 @@ namespace DDPM.EABroker
                 //Check for Span across multiple monitors
                 //
                 //1 Save original settings
-                bool orgSpanEnabled = _vm.IsSpanEnabled;
-                bool newSpanEnabled = orgSpanEnabled;
+                bool orgSpanEnabled = _vm.IsSpanEnabled;                
 
                 //2 Refresh settings
                 _vm.DetectSpanCondition();
                 //3 Check if changed
-                newSpanEnabled = _vm.IsSpanEnabled;
+                bool newSpanEnabled = _vm.IsSpanEnabled;
 
                 //4 Notify to UI if it's changed
                 if (newSpanEnabled != orgSpanEnabled)
