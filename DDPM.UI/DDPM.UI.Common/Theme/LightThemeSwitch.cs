d0fdd2dd6819c7d3abaf1ff3152d192a8f249e0c
brucechuang <brucechuang@wits.com>
2024-12-23 21:56:56 +0800

    [UI] Change color

--- DDPM.UI/DDPM.UI.Common/Theme/LightThemeSwitch.cs
+++ DDPM.UI/DDPM.UI.Common/Theme/LightThemeSwitch.cs
@@ -405,7 +405,7 @@ namespace DDPM.UI.Common
                 #endregion
 
                 UpdateBitmapImage("InterruptScreen_Image", new Uri($"pack://application:,,,/DDPM.UI.Common;component/Resources/Background_InterruptScreen_Light.png", UriKind.RelativeOrAbsolute));
-                UpdateFreezable("InterruptScreen_Backgroung", (ref SolidColorBrush brush) => brush.Color = (Color)ColorConverter.ConvertFromString("#FFFFFF"));
+                UpdateFreezable("InterruptScreen_Backgroung", (ref SolidColorBrush brush) => brush.Color = (Color)ColorConverter.ConvertFromString("#FFFFFFF2"));
 
                 BitmapImageUpdated?.Invoke(OSThemeEnum.Light);
             }
