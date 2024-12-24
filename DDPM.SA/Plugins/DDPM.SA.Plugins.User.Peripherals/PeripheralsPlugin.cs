b92e99df5cd64057020ad2ace009a896f660a8e2
Hess Cheng <HessCheng@wits.com>
2024-12-24 15:08:06 +0800

    [SA/UI] update EOL name

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.Peripherals/PeripheralsPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.Peripherals/PeripheralsPlugin.cs
@@ -12,6 +12,7 @@
 
 using DDPM.SA.Common;
 using DDPM.SA.Common.Settings;
+using DDPM.SA.Common.UI;
 using DDPM.SA.Resources.Helper;
 using Dell.Client.Framework.Common;
 using Dell.Client.Framework.Common.Annotations;
@@ -31,6 +32,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media.Media3D;
 using VcpCore.Common;
 using Windows.ApplicationModel;
 using IDeviceManager = IndiLogic.DPeM.Broker.IDeviceManager;
@@ -2704,6 +2706,8 @@ namespace DDPM.SA.Plugins.PeripheralsPlugin
                         {
                             OSDType_Device type = OSDType_Device.Unknown;
                             var deviceType = deviceInfo.LogicalDeviceType.ToUpper();
+                            var model = SACommonHelper.MappingModel(deviceInfo.ModelNumber);
+                            var message = $"{deviceInfo.Name.Replace(deviceInfo.ModelNumber, "").Trim()} {model}";
                             if (deviceType.Contains("PEN"))
                             {
                                 if (deviceInfo.ModelNumber == "PN5122W" && deviceInfo.BatteryLevel > 6)
@@ -2722,14 +2726,25 @@ namespace DDPM.SA.Plugins.PeripheralsPlugin
                             {
                                 type = OSDType_Device.Headset;
                             }
+                            else if (SACommonHelper.EOLKBList.Contains(deviceInfo.ModelNumber))
+                            {
+                                type = OSDType_Device.Keyboard;
+                                message = SACommonHelper.MappingEOLName(model);
+                            }
+                            else if (SACommonHelper.EOLMouseList.Contains(deviceInfo.ModelNumber))
+                            {
+                                type = OSDType_Device.Mouse;
+                                message = SACommonHelper.MappingEOLName(model);
+                            }
+
                             //_ = _DeviceManagerPlugin.ShowOSD(Screen.PrimaryScreen.DeviceName, OSDType.BatteryLow, type, deviceInfo.Name);
-                            OSDEventArgs args = new OSDEventArgs()
+                            OSDEventArgs args = new()
                             {
                                 Requester = "BatteryLow",
                                 DeviceName = Screen.PrimaryScreen.DeviceName,
                                 osd_type = OSDType.BatteryLow,
                                 osd_device = type,
-                                Message = deviceInfo.Name
+                                Message = message
                             };
                             OnOSDNotify(args);
                             Debug.WriteLine($"Show BatteryLow OSD: ID: {deviceInfo.ID} Level: {deviceInfo.BatteryLevel}");
@@ -2835,6 +2850,8 @@ namespace DDPM.SA.Plugins.PeripheralsPlugin
                         {
                             OSDType_Device type = OSDType_Device.Unknown;
                             var deviceType = deviceInfo.LogicalDeviceType.ToUpper();
+                            var model = SACommonHelper.MappingModel(deviceInfo.ModelNumber);
+                            var message = $"{deviceInfo.Name.Replace(deviceInfo.ModelNumber, "").Trim()} {model}";
                             if (deviceType.Contains("PEN"))
                             {
                                 if (deviceInfo.ModelNumber == "PN5122W" && arg2 > 6)
@@ -2853,6 +2870,17 @@ namespace DDPM.SA.Plugins.PeripheralsPlugin
                             {
                                 type = OSDType_Device.Headset;
                             }
+                            else if (SACommonHelper.EOLKBList.Contains(deviceInfo.ModelNumber))
+                            {
+                                type = OSDType_Device.Keyboard;
+                                message = SACommonHelper.MappingEOLName(model);
+                            }
+                            else if (SACommonHelper.EOLMouseList.Contains(deviceInfo.ModelNumber))
+                            {
+                                type = OSDType_Device.Mouse;
+                                message = SACommonHelper.MappingEOLName(model);
+                            }
+
                             //_ = _DeviceManagerPlugin.ShowOSD(Screen.PrimaryScreen.DeviceName, OSDType.BatteryLow, type, deviceInfo.Name);
                             OSDEventArgs args = new OSDEventArgs()
                             {
@@ -2860,7 +2888,7 @@ namespace DDPM.SA.Plugins.PeripheralsPlugin
                                 DeviceName = Screen.PrimaryScreen.DeviceName,
                                 osd_type = OSDType.BatteryLow,
                                 osd_device = type,
-                                Message = deviceInfo.Name
+                                Message = message
                             };
                             OnOSDNotify(args);
                             Debug.WriteLine($"Show BatteryLow OSD: ID: {deviceInfo.ID} Level: {deviceInfo.BatteryLevel}");
