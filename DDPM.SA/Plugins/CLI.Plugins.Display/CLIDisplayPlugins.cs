9d1b0554b175af61b671c9f85c841f40185153a1
Sheng-Chieh Chou <jc800703@hotmail.com>
2024-12-24 19:55:50 +0800

    [SA] Remove this silly bit operation.

--- DDPM.SA/Plugins/CLI.Plugins.Display/CLIDisplayPlugins.cs
+++ DDPM.SA/Plugins/CLI.Plugins.Display/CLIDisplayPlugins.cs
@@ -12068,7 +12068,7 @@ namespace DDPM.CLI.Plugins.Display
                 case "OSDENABLE": return (value | 0x4000).ToString();                   // b14:1;
                 case "OSDUNLOCK": return (value & 0x7FFF).ToString();                 // b15:0;
                 case "OSDLOCK": return (value | 0x8000).ToString();                   // b15:1;
-                case "OSDUNLOCK,OSDDISABLE": return ((value & 0x3FFF) | 0x0000).ToString();//b15 b14: 00
+                case "OSDUNLOCK,OSDDISABLE": return ((value & 0x3FFF) /*| 0x0000*/).ToString();//b15 b14: 00
                 case "OSDUNLOCK,OSDENABLE": return ((value & 0x3FFF) | 0x4000).ToString(); //b15 b14: 01
                 case "OSDLOCK,OSDDISABLE": return ((value & 0x3FFF) | 0x8000).ToString(); //b15 b14: 10
                 case "OSDLOCK,OSDENABLE": return ((value & 0x3FFF) | 0xC000).ToString();  //b15 b14: 11
