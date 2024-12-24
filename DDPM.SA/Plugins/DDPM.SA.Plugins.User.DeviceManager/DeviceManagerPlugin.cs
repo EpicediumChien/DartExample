db3595dac4c62d9552de7761ed085e16bb8fdd03
Sheng-Chieh Chou <jc800703@hotmail.com>
2024-12-24 20:02:40 +0800

    [SA] Remove this silly bit operation.

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
@@ -14496,7 +14496,7 @@ namespace DDPM.SA.Plugins.User.DeviceManager
                     }
                     else
                     {
-                        bool ret = SetVCPCapability(monitorInfo, 0xE0, (0 | (uint)getvalue)).Result;
+                        bool ret = SetVCPCapability(monitorInfo, 0xE0, (/*0 |*/ (uint)getvalue)).Result;
                         writelog($"PowerNap ReduceBrightness:[{monitorInfo.edid.ModelName}:{monitorInfo.edid.SerialNumber}] OFF and setVcp:]" + (ret ? "success" : "fail"));
                     }
                 }
@@ -14539,7 +14539,7 @@ namespace DDPM.SA.Plugins.User.DeviceManager
                     }
                     else
                     {
-                        bool ret = SetVCPCapability(monitorInfo, 0xE0, (0 | (uint)getvalue)).Result;
+                        bool ret = SetVCPCapability(monitorInfo, 0xE0, (/*0 |*/ (uint)getvalue)).Result;
                         writelog($"PowerNap SuspendMonitor:[{monitorInfo.edid.ModelName}:{monitorInfo.edid.SerialNumber}] OFF and setVcp:]" + (ret ? "success" : "fail"));
                     }
                 }
