203978e546d85f515b571f93462eab9664a3c44e
Sheng-Chieh Chou <jc800703@hotmail.com>
2024-12-30 18:42:11 +0800

    [UI] Use the 'value' parameter in this property set accessor declaration.

--- DDPM.UI/DDPM.UI.Plugin.Common/ViewModels/WebCameraViewModel.cs
+++ DDPM.UI/DDPM.UI.Plugin.Common/ViewModels/WebCameraViewModel.cs
@@ -1030,7 +1030,7 @@ namespace DDPM.UI.Plugin.ViewModels
         {
             get => IsProximitySensorEnable;
 
-            set { }
+            //set { }
         }
 
         private bool _isChecked_Autofocus;
