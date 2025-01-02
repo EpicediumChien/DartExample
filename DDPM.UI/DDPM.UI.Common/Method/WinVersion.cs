1ac6411ef1386422962979f6674e47a0580c4119
Sheng-Chieh Chou <jc800703@hotmail.com>
2025-01-02 17:01:34 +0800

    [UI] Either change the type of 'mo' to 'ManagementBaseObject' or iterate on a generic collection of type 'ManagementObject'.

--- DDPM.UI/DDPM.UI.Common/Method/WinVersion.cs
+++ DDPM.UI/DDPM.UI.Common/Method/WinVersion.cs
@@ -99,7 +99,7 @@ namespace DDPM.UI.Common.Method
             ManagementObjectCollection moc = mc.GetInstances();
             if (moc.Count != 0)
             {
-                foreach (ManagementObject mo in mc.GetInstances())
+                foreach (ManagementObject mo in moc.Cast<ManagementObject>())
                 {
                     try
                     {
