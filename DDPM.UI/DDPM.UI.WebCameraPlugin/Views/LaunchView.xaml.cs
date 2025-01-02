b918c9f74da40d1c76f4cf52065db2d93cbfd1cb
Leo Lin <LeoLin@wits.com>
2025-01-02 14:42:28 +0800

    [UI] PIMS-PIMS-325273 FIXED

--- DDPM.UI/DDPM.UI.WebCameraPlugin/Views/LaunchView.xaml.cs
+++ DDPM.UI/DDPM.UI.WebCameraPlugin/Views/LaunchView.xaml.cs
@@ -424,6 +424,10 @@ namespace DDPM.UI.Plugin.WebCameraPlugin
                             _vm.brdHello_show = Visibility.Collapsed;
                             _vm.MPS_Setting_Visibility = Visibility.Collapsed;
                             _vm.MPS_UpdateFW_Visibility = Visibility.Visible;
+
+                            //2025/01/02 fixed
+                            noPresenceFunction = true;
+
                         }
 
                         //dell 7 韌體升級畫面需要在 usb 3.0下,如果在2.0模式整個分頁關閉
