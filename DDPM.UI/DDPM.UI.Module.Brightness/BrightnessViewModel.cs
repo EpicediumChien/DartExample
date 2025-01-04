ef5867a918ff37803b48fb9b04b44c2ac915055f
Wayn_chen <Wayn_chen@wistron.com>
2025-01-04 21:26:02 +0800

    讓ALS的Busy同步

--- DDPM.UI/DDPM.UI.Module.Brightness/BrightnessViewModel.cs
+++ DDPM.UI/DDPM.UI.Module.Brightness/BrightnessViewModel.cs
@@ -444,6 +444,13 @@ namespace DDPM.UI.Module.Brightness
             NotifyPropertyChanged(nameof(BrightnessEnable));
         }
 
+        public bool AreAllConfigsNotBusy(List<ALSConfig> configs)
+        {
+            if (configs == null || configs.Count == 0)
+                return true; 
+
+            return configs.All(config => !config.isBusy);
+        }
         /// <summary>
         /// Catch OSD menu event
         /// </summary>
@@ -477,8 +484,16 @@ namespace DDPM.UI.Module.Brightness
 
                     Start_ALSConfig = tmp[idx];
 
+                    IsBusyALS = true;
+                    NotifyPropertyChanged("IsBusyALS");
+
                     //2.if yes, then update the vcp value to each option
                     GetALSContentAndSyncUI(SelectedHomeDevice.MonitorInfo);
+                    IsBusyALS = !AreAllConfigsNotBusy(tmp);
+                    if (!IsBusyALS)
+                        NotifyPropertyChanged("IsBusyALS");
+                    DdpmCommonHelper.WriteUILog($"[OnVCPChangedEvent][BrightnessViewModel] ModelName = {SelectedHomeDevice.MonitorInfo.modelName}, IsBusyALS = {IsBusyALS.ToString()}");
+
                 }
                 catch (Exception ex)
                 {
@@ -3844,7 +3859,27 @@ namespace DDPM.UI.Module.Brightness
             get => _isBusy;
             set => SetProperty(ref _isBusy, value);
         }
+        private bool _isBusyALS = false;
 
+        public bool IsBusyALS
+        {
+            get => _isBusyALS;
+            set => SetProperty(ref _isBusyALS, value);
+        }
+        public void SetIsBusy(bool busy)
+        {
+            List<ALSConfig> tmp = DdpmCommonHelper.DeviceManagerSA.GetAllExistAlsConfig().Result;
+            if (tmp == null || tmp.Count <= 1)
+                return;
+
+            IsBusyALS = true;
+            NotifyPropertyChanged("IsBusyALS");
+
+            IsBusyALS = !AreAllConfigsNotBusy(tmp);
+            if (!IsBusyALS)
+                NotifyPropertyChanged("IsBusyALS");
+            DdpmCommonHelper.WriteUILog($"[SetIsBusy][BrightnessViewModel] ModelName = {SelectedHomeDevice.MonitorInfo.modelName}, IsBusyALS = {IsBusyALS.ToString()}");
+        }
         #endregion UI Enable Flags
     }
 }
