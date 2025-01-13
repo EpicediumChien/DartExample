156d7e6529c441da9eff1fcab18912083fda56a1
Robert_Lin <Robert_Lin@wistron.com>
2025-01-13 11:31:46 +0800

    Fix build error, missing a "}"

--- DDPM.SA/Common/ColorApp/MonitorWin.xaml.cs
+++ DDPM.SA/Common/ColorApp/MonitorWin.xaml.cs
@@ -270,35 +270,36 @@ namespace DDPM.ColorApp
                     catch (Exception ex)
                     {
                         writelog($"[EventAppStatus_SendValue(] FindScreen {ex.Message} ");
-                    }                    
+                    }
 
-                    if (Mi != null && screen!= null && s != null) // Jim 20250108 add if null check
+                    if (Mi != null && screen != null && s != null) // Jim 20250108 add if null check
                     {
                         Trace.WriteLine("Mi.DisplayName = " + Mi.DisplayName);
                         Trace.WriteLine("Mi.modelName = " + Mi.modelName);
 
 
-                    Trace.WriteLine("screen.WorkingArea.Height = " + screen.WorkingArea.Height);
-                    Trace.WriteLine("s.WorkingArea.Height = " + s?.WorkingArea.Height);
+                        Trace.WriteLine("screen.WorkingArea.Height = " + screen.WorkingArea.Height);
+                        Trace.WriteLine("s.WorkingArea.Height = " + s?.WorkingArea.Height);
 
-                    Trace.WriteLine("screen.WorkingArea.Width = " + screen.WorkingArea.Width);
-                    Trace.WriteLine("s.WorkingArea.Width = " + s?.WorkingArea.Width);
+                        Trace.WriteLine("screen.WorkingArea.Width = " + screen.WorkingArea.Width);
+                        Trace.WriteLine("s.WorkingArea.Width = " + s?.WorkingArea.Width);
 
-                    Trace.WriteLine("screen.WorkingArea.Left = " + screen.WorkingArea.Left);
-                    Trace.WriteLine("s.WorkingArea.Left = " + s?.WorkingArea.Left);
+                        Trace.WriteLine("screen.WorkingArea.Left = " + screen.WorkingArea.Left);
+                        Trace.WriteLine("s.WorkingArea.Left = " + s?.WorkingArea.Left);
 
                         writelog("Mi.DisplayName = " + Mi.DisplayName);
                         writelog("Mi.modelName = " + Mi.modelName);
 
 
-                    writelog("screen.WorkingArea.Height = " + screen.WorkingArea.Height);
-                    writelog("s.WorkingArea.Height = " + s?.WorkingArea.Height);
+                        writelog("screen.WorkingArea.Height = " + screen.WorkingArea.Height);
+                        writelog("s.WorkingArea.Height = " + s?.WorkingArea.Height);
 
-                    writelog("screen.WorkingArea.Width = " + screen.WorkingArea.Width);
-                    writelog("s.WorkingArea.Width = " + s?.WorkingArea.Width);
+                        writelog("screen.WorkingArea.Width = " + screen.WorkingArea.Width);
+                        writelog("s.WorkingArea.Width = " + s?.WorkingArea.Width);
 
-                    writelog("screen.WorkingArea.Left = " + screen.WorkingArea.Left);
-                    writelog("s.WorkingArea.Left = " + s?.WorkingArea.Left);
+                        writelog("screen.WorkingArea.Left = " + screen.WorkingArea.Left);
+                        writelog("s.WorkingArea.Left = " + s?.WorkingArea.Left);
+                    }
 
                     //if (s == null)//Dean 0626 fix SAST issue
                     //    return;
