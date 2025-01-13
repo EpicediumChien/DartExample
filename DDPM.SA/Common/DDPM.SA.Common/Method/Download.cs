e1ae9151ce16760a938bff362c046de53cc66c7c
Dean Yang(WISTRON) <dean_yang@wistron.com>
2025-01-13 21:10:17 +0800

    update

--- DDPM.SA/Common/DDPM.SA.Common/Method/Download.cs
+++ DDPM.SA/Common/DDPM.SA.Common/Method/Download.cs
@@ -1,4 +1,6 @@
 ﻿using DDPM.SA.Common.Security;
+using DDPM.SA.Common.Settings;
+using Microsoft.Win32;
 using System;
 using System.IO;
 using System.Net;
@@ -16,6 +18,29 @@ namespace DDPM.SA.Common.Method
         public Download(Logs logs)
         {
             _logs = logs;
+        }
+
+        public static string GetTestServerURL()
+        {
+            RegistryKey localKey64 = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
+            string ret = GlobalDefinitions.major_url;//this is default value (production server)
+            if (localKey64 != null)
+            {
+                RegistryKey registryKey = localKey64.OpenSubKey("SOFTWARE\\Dell\\DDPM Subagent\\", false);
+                if (registryKey != null)
+                {
+                    var obj = registryKey?.GetValue("TestServerURL");
+                    if (obj != null)
+                    {
+                        string s = obj.ToString();
+                        if (!string.IsNullOrEmpty(s))
+                        {
+                            ret = s;
+                        }
+                    }
+                }
+            }
+            return ret;
         }
 
         public bool DownloadFile(string URLPath, string SavePath, out string FailInfo, bool isSkipCA = false)
