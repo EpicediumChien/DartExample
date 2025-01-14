11121c0f0ce92600c5f2ad8a924fac038b246640
Rex_Cai@wistron.com <Rex_Cai@wistron.com>
2025-01-14 09:53:58 +0800

    Update comment

--- DDPM.SA/Plugins/CLI.Plugins.Display/CLIDisplayPlugins.cs
+++ DDPM.SA/Plugins/CLI.Plugins.Display/CLIDisplayPlugins.cs
@@ -9999,7 +9999,7 @@ namespace DDPM.CLI.Plugins.Display
                 }
             }
 
-            //If -ServiceTag=[{tag0}],[{tag1}],[{tag2}],... is specified
+            //If -model=[{model0}],[{model1}],[{model2}],... is specified
             if (cmdLineInput.Model.Count > 0)
             {
                 isAllMonitors = false;
@@ -10007,12 +10007,11 @@ namespace DDPM.CLI.Plugins.Display
                 {
                     if (!String.IsNullOrWhiteSpace(mi.modelName))
                     {
-                        //Check if this monitor's service tag in in the -ServiceTag list
+                        //Check if this monitor's model name in in the -model list
                         string? match = cmdLineInput.Model.FirstOrDefault(x => x.Equals(mi.modelName, StringComparison.OrdinalIgnoreCase));
                         if (match != null) //If found, add index value to listOut
                             listOut.Add(mi.Index);
                     }
-                    //Empty ServiceTage will not be added
                 }
             }
             //If -Index=[{idx0}],[{idx1}],[{idx2}],... is specified
