824b79aac94835e63c4b96c880fddfd4c7ae8b66
Sheng-Chieh Chou <jc800703@hotmail.com>
2025-01-15 16:08:34 +0800

    [SA] Update FileInfo check

--- DDPM.SA/Common/DDPM.SA.Common/Settings/DDPMFileSecurity.cs
+++ DDPM.SA/Common/DDPM.SA.Common/Settings/DDPMFileSecurity.cs
@@ -123,12 +123,17 @@ namespace DDPM.SA.Common.Settings
             }
 
             //Dean 1225 add Security code to drop data save if path include symlink
-            FileInfo fInfo = new FileInfo(target_file);
-            if(!fInfo.Exists)
+            FileInfo fInfo = default;
+            try
+            {
+                fInfo = new FileInfo(target_file);
+            }
+            catch(Exception ex)
             {
                 info = $"[SetJsonContentFromSerializedString] can't create FileInfo via target_file";
                 return false;
             }
+
             if(string.IsNullOrEmpty(fInfo.DirectoryName))
             {
                 info = $"[SetJsonContentFromSerializedString] retrieve file's folder via target_file fail";
