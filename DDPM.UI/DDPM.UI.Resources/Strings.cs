f733fabd9a367532237d5df7f270af19b546c819
Robert_Lin <Robert_Lin@wistron.com>
2025-01-18 17:16:00 +0800

    PIMS-332075 es-ES, es-XL, fr-CA, it-IT, pl-PL, pt-BR, pt-PT: StringID: Keyboard.2 was clipped PIMS-333759 de-DE, es-ES, es-XL, fr-CA, it-IT, pl-PL, pt-PT, pt-BR, ru-RU, tr-TR, ua-UK: Clipping found in  stringID: WalkThroughMouseMS355_Main.1 PIMS-339102 Observe no Progress Bar on FW update in localised language PIMS-333922 es-ES, es-XL, it-IT, pt-BR, pt-PT, ua-UK: Clipping found in stringID: Color.5

--- DDPM.UI/DDPM.UI.Resources/Strings.cs
+++ DDPM.UI/DDPM.UI.Resources/Strings.cs
@@ -1,5 +1,5 @@
 ﻿//Robert_Lin 2025-1-14, to simple swich betwwen Teesting and Release mode, please comment out the following line in Release mode
-#define MULTILINGUAL_TEST
+//#define MULTILINGUAL_TEST
 
 using System.Globalization;
 using System.Windows.Controls.Primitives;
