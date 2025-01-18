b486766d847060de2c63720a6796b24187f094b0
brucechuang <brucechuang@wits.com>
2025-01-18 12:16:51 +0800

    [SA] 1.Add arguments deviceID to fix RF mode FWU

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.FWUpdate/FWUpdatePlugins.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.FWUpdate/FWUpdatePlugins.cs
@@ -515,6 +515,7 @@ namespace DDPM.SA.Plugins.User.FWUpdate
                                     updateHelper.UpdateItems[i].DeviceType == DeviceType.LogicalDock)),
                                     IsDisplay = false,
                                     SupplierID = deviceSupplierID,
+                                    InstanceId = updateHelper.UpdateItems[i].InstanceId,
                                     Connectivity = deviceConnectivity,
                                     Available_date = _fWUpdateInfoPackage.TheLastCheckTime.ToString("yyyy/MM/dd HH:mm:ss"),
                                     ServiceTag = ((updateHelper.UpdateItems[i].DeviceType == DeviceType.PhysicalWiredDock ||
@@ -558,6 +559,7 @@ namespace DDPM.SA.Plugins.User.FWUpdate
                                         updateHelper.UpdateItems[i].DeviceType == DeviceType.LogicalDock)),
                                         IsDisplay = false,
                                         SupplierID = deviceSupplierID,
+                                        InstanceId = updateHelper.UpdateItems[i].InstanceId,
                                         Connectivity = deviceConnectivity,
                                         Available_date = _fWUpdateInfoPackage.TheLastCheckTime.ToString("yyyy/MM/dd HH:mm:ss"),
                                         ServiceTag = ((updateHelper.UpdateItems[i].DeviceType == DeviceType.PhysicalWiredDock ||
@@ -2578,6 +2580,15 @@ namespace DDPM.SA.Plugins.User.FWUpdate
                         case "RF":
                             arguments += $" /updatepath:RF";
                             _logs.DebugMsg_1($"BuildArgs Add : /updatepath:RF");
+                            _logs.DebugMsg_1($"GetDeviceID go");
+                            string deviceID = GetDeviceID(fwUpdateInfo);
+                            _logs.DebugMsg_1($"GetDeviceID done");
+                            if (!string.IsNullOrEmpty(deviceID))
+                            {
+                                _logs.DebugMsg_1($"GetDeviceID deviceID is not null");
+                                _logs.DebugMsg_1($"BuildArgs Add :{deviceID}");
+                                arguments += deviceID;
+                            }
                             break;
                         case "Bluetooth":
                             arguments += $" /updatepath:BLE";
@@ -2676,6 +2687,66 @@ namespace DDPM.SA.Plugins.User.FWUpdate
             }
             _logs.DebugMsg_1($"Check_CanBeOTAUpdate finish. ret : {ret}");
             return ret;
+        }
+        string GetDeviceID(FWUpdateInfo fwUpdateInfo)
+        {
+            _logs.DebugMsg_1($"GetDeviceID start");
+            string ret = string.Empty;
+            try
+            {
+                if (fwUpdateInfo.DeviceType != DeviceType.LogicalHeadset)
+                {
+                    // 提取 deviceId 並將三個字節組合起來，作為參數傳遞給 FW 更新程序
+                    int deviceId = fwUpdateInfo.InstanceId;
+
+                    byte device_id_1 = (byte)(deviceId & 0xFF);
+                    byte device_id_2 = (byte)((deviceId >> 8) & 0xFF);
+                    byte device_id_3 = (byte)((deviceId >> 16) & 0xFF);
+
+                    // 組合三個字節
+                    ret = $" /deviceID:{device_id_1:X2}{device_id_2:X2}{device_id_3:X2}";
+                }
+                else
+                {
+                    // 對於耳機，將 instance id 視為 deviceHandle 並傳遞給耳機的 FW 更新程序
+                    int deviceHandleId = fwUpdateInfo.InstanceId;
+
+                    ret = $" /deviceID:{deviceHandleId:X4}";
+                }
+            }
+            catch (Exception ex)
+            {
+                _logs.DebugMsg_1($"GetDeviceID eroor : {ex.Message}");
+            }
+            _logs.DebugMsg_1($"GetDeviceID ret : {ret}");
+            _logs.DebugMsg_1($"GetDeviceID done");
+            return ret;
+            /*IL Sample
+            if (updaterItemModel->_deviceType != IDevice::DeviceTypeLogicalHeadset) // Check if device is not headset type
+            {
+                //
+                // Extract the deviceId and combine the three bytes to pass to FW updater as arguments.
+                //
+                int deviceId = updaterItemModel->_instanceId;
+
+                uchar device_id_1 = (deviceId & 0xFF);
+                uchar device_id_2 = ((deviceId >> 8) & 0xFF);
+                uchar device_id_3 = ((deviceId >> 16) & 0xFF);
+
+                //
+                // combine the 3 bytes
+                //
+                arguments << ("/deviceID:" + QString("%1").arg(QString::number(device_id_1, 16), 2, QChar('0')) + QString("%1").arg(QString::number(device_id_2, 16), 2, QChar('0')) + QString("%1").arg(QString::number(device_id_3, 16), 2, QChar('0')));
+            }
+            else
+            {
+                //
+                // For headset treat the instance id as deviceHandle and pass it to FW updater of headset
+                //
+                int deviceHandleId = updaterItemModel->_instanceId;
+
+                arguments << ("/deviceID:" + QString("%1").arg(QString::number(deviceHandleId, 16), 4, QChar('0')));
+            }*/
         }
         /// <summary>
         /// Rearrange firmware update order
