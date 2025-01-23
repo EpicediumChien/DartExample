1421b2cf58a7f1b580a7537f0401f60c6806ca90
Dean Yang(WISTRON) <dean_yang@wistron.com>
2025-01-23 22:08:13 +0800

    add global param and static function for log output

--- DDPM.SA/Common/DDPM.SA.Common/Settings/GlobalDefinitions.cs
+++ DDPM.SA/Common/DDPM.SA.Common/Settings/GlobalDefinitions.cs
@@ -14,5 +14,29 @@ namespace DDPM.SA.Common.Settings
         public const string SW_URL_Folder = @$"/Windows/Application/";
         public const string Dongle_BeforeGen2_Name = "Dell Universal Receiver";
         public const string Display_ICC_URL_Folder = @"ICC/";
+
+        //for log print comparison
+        private const string production_server = "clientperipherals.dell.com";
+        private const string staging_server = "clientperipherals-uat.dell.com";
+        private const string download_server = "downloads.dell.com";
+
+        public static string GetLogPrintServerName(string url)
+        {
+            if (url.Contains(production_server, StringComparison.OrdinalIgnoreCase))
+            {
+                return "PRODUCTION SERVER";
+            }
+            else if (url.Contains(staging_server, StringComparison.OrdinalIgnoreCase))
+            {
+                return "STAGING SERVER";
+            }
+            else if (url.Contains(download_server, StringComparison.OrdinalIgnoreCase))
+            {
+                return "DELL DOWNLOADS SERVER";
+            }
+            else
+            {
+                return "Unknown address";
+            }
+        }
     }
-}
--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.FWUpdate/FWUpdatePlugins.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.FWUpdate/FWUpdatePlugins.cs
@@ -1009,22 +1009,7 @@ namespace DDPM.SA.Plugins.User.FWUpdate
                         // 將儲存路徑與從 URL 中提取的檔案名稱組合
                         if (_IsSkipSHA)
                         {
-                            string outputUrlForLog = "others";
-
-                            if (url.Contains("clientperipherals.dell.com"))
-                            {
-                                outputUrlForLog = "production_server";
-                            }
-                            else if (url.Contains("clientperipherals-uat.dell.com"))
-                            {
-                                outputUrlForLog = "staging_server";
-                            }
-                            else if (url.Contains("download.dell.com"))
-                            {
-                                outputUrlForLog = "dell_download";
-                            }
-
-                            _logs.DebugMsg_1($"{nameof(DownloadAndInstall)} ServerPath : {outputUrlForLog}");
+                            _logs.DebugMsg_1($"{nameof(DownloadAndInstall)} ServerPath : {GlobalDefinitions.GetLogPrintServerName(url)}");
                         }
                         _installationFileStoragePath = Path.Combine(savePath + Path.GetFileName(url));
                         _logs.DebugMsg_1($"{nameof(DownloadAndInstall)} download.DownloadFile go");
