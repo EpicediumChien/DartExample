b404464470367878d4c42365755596e22bb80074
Hess Cheng <HessCheng@wits.com>
2025-01-25 09:33:26 +0800

    fix PIMS-337175 Proximity Sensor Option goes back to On after access Device Setting Menu

--- DDPM.SA/Common/DDPM.SA.Common/Settings/WebcamSettings.cs
+++ DDPM.SA/Common/DDPM.SA.Common/Settings/WebcamSettings.cs
@@ -451,22 +451,6 @@ namespace DDPM.SA.Common.Settings
                 //Init a new data
                 if (tmp == null)
                 {
-
-                    //add by leo 2025/01/16
-                    //Creating a profile for the first time
-                    //ProximitySensor status is restored to original factory initialization to Off.
-                    if (devMgr != null)
-                    {
-                        devMgr.SetIsProximitySensorEnable(di.ID.ToString(), false);
-                    }
-                    else
-                    {
-                        // 處理 devMgr 為 null 的情況
-                        log?.Info("Device manager is not initialized.");
-                    }
-
-
-                    log?.Info(@$"[WebcamSettings][ImportWebcamSettings] init via di(jsonString:{JsonConvert.SerializeObject(di)})");
                     tmp = new WebcamSettings(di, devMgr, log);
                     tmp = ReAlignWebcamResolution(tmp, model, di, devMgr, log);
                 }
