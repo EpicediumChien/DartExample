46356e5bf194d10f3f08e313729c784cafbb79b8
Hess Cheng <HessCheng@wits.com>
2025-02-04 15:38:45 +0800

    fix PIMS-330887 All DPeM settings are changed and actions assigned for DUT. Check settings in DDPM of the DUT is not the same as what was set in Preinfo Step 2 for DPeM

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.DTPProxy/DTPProxyPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.DTPProxy/DTPProxyPlugin.cs
@@ -3730,7 +3730,7 @@ namespace DDPM.SA.Plugins.User.DTPProxy
                 "Speaker" => _speakerMethodInfo,
                 "Dongle" => _dongleMethodInfo,
                 "Dock" => _dockMethodInfo,
-                "AirAudio"=> _airaudioMethodInfo,
+                "AirAudio" => _airaudioMethodInfo,
                 _ => null
             };
             Type interfaceType = type switch
@@ -3776,7 +3776,6 @@ namespace DDPM.SA.Plugins.User.DTPProxy
 
                     if (value == null)
                     {
-                        Debug.WriteLine($"Not found {type} GUID: {guid}");
                         writelog($"Not found {type} GUID: {guid}");
                         return false;
                     }
@@ -9890,7 +9889,7 @@ namespace DDPM.SA.Plugins.User.DTPProxy
                 text = "";
 
             text = $"[DTPProxyPlugin] {text}, Caller Name:{memberName}, Source Line {sourceLineNumber}";
-            Console.WriteLine(text);
+            Debug.WriteLine(text);
 
 
             if (Log != null)
