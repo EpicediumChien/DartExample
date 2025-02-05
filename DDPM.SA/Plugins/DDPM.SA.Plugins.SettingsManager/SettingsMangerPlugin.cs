43bb17e30ba3e9fad02d61b3b3f9c1b3478b47c8
Dean Yang(WISTRON) <dean_yang@wistron.com>
2025-02-05 19:35:20 +0800

    [SA] cover intaller to delete DDM2 folder with security check

--- DDPM.SA/Plugins/DDPM.SA.Plugins.SettingsManager/SettingsMangerPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.SettingsManager/SettingsMangerPlugin.cs
@@ -98,6 +98,21 @@ namespace DDPM.SA.Plugins.SettingsManager
                     WriteLog($"SettingsManagerPlugin constructor ... ACL failed...{info}");
                 }
             }
+
+            string DDM2Path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), @"Dell\Dell Display Manager 2");
+            if (Directory.Exists(DDM2Path))
+            {
+                WriteLog($"SettingsManagerPlugin constructor ... DDM2Path exist, check and delete it");
+                if (DDPMFileSecurity.ValidateFilePath(DDM2Path, out string info))
+                {
+                    WriteLog($"SettingsManagerPlugin constructor ... DDM2Path is valid, delete it");
+                    Directory.Delete(DDM2Path, true);
+                }
+                else
+                {
+                    WriteLog($"SettingsManagerPlugin constructor ... DDM2Path is invalid, skip delete it ({info})");
+                }
+            }
         }
 
         #endregion
