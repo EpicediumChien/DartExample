254c63b7040c00d7df4a7be022ab706b52c5e48f
Wayn_chen <Wayn_chen@wistron.com>
2025-02-06 20:13:45 +0800

    Modify Headset loading time

--- DDPM.UI/DDPM.UI.Plugin.Common/ViewModels/HeadsetViewModel.cs
+++ DDPM.UI/DDPM.UI.Plugin.Common/ViewModels/HeadsetViewModel.cs
@@ -2082,7 +2082,7 @@ namespace DDPM.UI.Plugin.ViewModels
             {
                 FirmwareVersion2 = _deviceManager.GetAirAudioFirmwareVersionAsync(CurrentDeviceID.ToString()).Result;
             }
-            Thread.Sleep(100);
+            Thread.Sleep(1000);
             if ((FirmwareVersion2 == null || FirmwareVersion2 == "0.0.0.0"))
             {
                 
