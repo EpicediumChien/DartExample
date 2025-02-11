84d8316cb1f7dac5b2c88efec03bdaa8ded07c74
brucechuang <brucechuang@wits.com>
2025-02-11 22:44:50 +0800

    Add log

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.DisplayManager/DisplayMangerPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.DisplayManager/DisplayMangerPlugin.cs
@@ -4092,9 +4092,6 @@ namespace DDPM.SA.Plugins.User.DisplayManager
                 string manufacturer_Name = Vendor_Product_Identification.Manufacturer_Name(edid_byte);
                 string product_Id = Vendor_Product_Identification.Product_Id(edid_byte).PadLeft(4, '0');
                 MonitorInfo? monitorInfo = monitorInfos.Find(o => BitConverter.ToString((byte[])currentMnoitorByteArr[i]).Replace("-", "").StartsWith(o.edid.Edid));
-                _logs.DebugMsg($"[DisplayMangerPlugin] {nameof(GetMonitorAssetReport)} monitorInfo is null :{monitorInfo == null}");
-                Debug.WriteLine(monitorInfos[0].edid.Edid);
-                Debug.WriteLine(BitConverter.ToString((byte[])currentMnoitorByteArr[i]).Replace("-", ""));
                 string modelName = $"{manufacturer_Name} {manufacturer_Name + product_Id}";
                 string manufacturer = manufacturer_Name;
                 string orientation = "N/A";
@@ -4106,6 +4103,7 @@ namespace DDPM.SA.Plugins.User.DisplayManager
                 string connection = "DisplayPort";
                 string serialNumber = "N/A";
                 string optimalResolution = "N/A";
+                _logs.DebugMsg($"[DisplayMangerPlugin] {nameof(GetMonitorAssetReport)} monitorInfo is null :{monitorInfo == null}");
                 if (monitorInfo != null)
                 {
                     modelName = monitorInfo.modelName;
