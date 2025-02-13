c4653601da7c7970a4c7c6a68e4cf762b14e0fc4
Dean Yang(WISTRON) <dean_yang@wistron.com>
2025-02-13 19:04:45 +0800

    align with Dell Wendy to keep original key.

--- DDPM.SA/Common/DDPM.SA.Common/FWUpdateInfoPackage.cs
+++ DDPM.SA/Common/DDPM.SA.Common/FWUpdateInfoPackage.cs
@@ -179,7 +179,7 @@ namespace DDPM.SA.Common
         public string Thumbprint { get; set; }
         public string url { get; set; }
         public string date { get; set; }
-        [JsonPropertyName("Supported_platform")]
+        [JsonPropertyName("support_platform")]
         public string SupportedPlatform { get; set; }
         public string ServiceTag { get; set; }
         public string SupplierID { get; set; }
