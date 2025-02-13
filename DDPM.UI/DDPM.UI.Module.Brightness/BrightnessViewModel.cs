98e68fe671168d1c529c2f1530b1254017b6704b
Sheng-Chieh Chou <jc800703@hotmail.com>
2025-02-13 16:50:16 +0800

    [UI] All return same string?

--- DDPM.UI/DDPM.UI.Module.Brightness/BrightnessViewModel.cs
+++ DDPM.UI/DDPM.UI.Module.Brightness/BrightnessViewModel.cs
@@ -3402,6 +3402,8 @@ namespace DDPM.UI.Module.Brightness
             {
                 if (Start_ALSConfig.AutoBrightnessRangeLevel.Count == 0)
                     return "";
+
+                //all return same string? no need to use if-else
                 if (Start_ALSConfig.AutoBrightnessRangeLevel[0].level_value == 0)
                     return LangHelper.Instance["BrightnessLevel"] + ": " + BrightnessValue.ToString() + "%";//40%";
                 else if (Start_ALSConfig.AutoBrightnessRangeLevel[0].level_value == 1)
