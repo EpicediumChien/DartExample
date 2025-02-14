d7b20a599291cc3015ee8f9ea5fa4a2c8b3c21d8
Wayn_chen <Wayn_chen@wistron.com>
2025-02-14 18:59:27 +0800

    Update

--- DDPM.UI/DDPM.UI.WebCameraPlugin/Views/LaunchView.xaml.cs
+++ DDPM.UI/DDPM.UI.WebCameraPlugin/Views/LaunchView.xaml.cs
@@ -119,7 +119,7 @@ namespace DDPM.UI.Plugin.WebCameraPlugin
                 InitializeComponent();
                 _vm.Reset();
                 DataContext = _vm;
-                _vm.VbarItemClickCommand = new RelayCommand<VbarItem>(OnVbarItemClicked!);
+                //_vm.VbarItemClickCommand = new RelayCommand<VbarItem>(OnVbarItemClicked!);
 
                 //leo 2024/12/09 因為多加條件判斷,改變呼叫位置
                 //BuildModuleGroups();
@@ -255,13 +255,14 @@ namespace DDPM.UI.Plugin.WebCameraPlugin
 
                 //因為需要處理PresenceDetection分頁是否出現判斷,改變呼叫順序
                 check_PresenceFunction();
-                BuildModuleGroups();
+                //BuildModuleGroups();
                 //initResolutionFPS();
                 //usb 2.0限制規則要放在最後做校正
                 CheckUSBtype();
 
                 //grdPreview.Visibility = _vm.WebcamGrid ? Visibility.Visible : Visibility.Hidden;
-
+                _vm.VbarItemClickCommand = new RelayCommand<VbarItem>(OnVbarItemClicked!);
+                BuildModuleGroups();
                 DdpmCommonHelper.BitmapImageUpdated += ImageUpdate;
             }
             catch (Exception ex)
