dd7be06c726aca33e7e0ea4f690506ec8c04e7a5
Kidd Huang <KiddHuang@wits.com>
2025-02-14 16:51:50 +0800

    [Fix] Fix PIMS-345755 [DDPM WIN 2.0][R21_webcam] U3223QZ name is incomplete when hovering over the DUT image on the home page.

--- DDPM.SA/Common/DDPM.SA.Common/UI/SAUICommonHelper.cs
+++ DDPM.SA/Common/DDPM.SA.Common/UI/SAUICommonHelper.cs
@@ -168,7 +168,7 @@ namespace DDPM.SA.Common.UI
                     return name;
                 default:
                     if (model == "U3224KB")                   
-                        return "Dell UltraSharp 32 6K Monitor U3224KB";
+                        return "Dell UltraSharp 32 6K Monitor";
                     return name;
             }
         }
