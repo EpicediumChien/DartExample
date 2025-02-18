6593458123c63e8073bd14ec5ffd6ffefa8248fe
Sheng-Chieh Chou <jc800703@hotmail.com>
2025-02-18 14:25:33 +0800

    [UpdateSupportedResolutions] WB5023 same as default?

--- DDPM.SA/Common/DDPM.SA.Common/Settings/WebcamSettings.cs
+++ DDPM.SA/Common/DDPM.SA.Common/Settings/WebcamSettings.cs
@@ -80,7 +80,7 @@ namespace DDPM.SA.Common.Settings
                 if (string.IsNullOrEmpty(str))
                 {
                     log?.Info("DTP GetSupportedResolutions fail!");
-                    switch (di.ModelNumber)
+                    switch (di.ModelNumber) // WB5023 same as default?
                     {
                         case "WB5023":
                             str = "[{\"Resolution\":\"1280x720\",\"FPS\":[\"24\",\"30\",\"60\"]},{\"Resolution\":\"1920x1080\",\"FPS\":[\"24\",\"30\",\"60\"]},{\"Resolution\":\"2560x1440\",\"FPS\":[\"24\",\"30\"]}]";
