1bab214dcd6c614d266a2495b259bca4a927ddb7
Wayn_chen <Wayn_chen@wistron.com>
2025-02-21 15:37:16 +0800

    Improve GetUserID Function

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
@@ -264,6 +264,7 @@ namespace DDPM.SA.Plugins.User.DeviceManager
         private bool isDownloadingICC = false;//Added 02/13 by Bruce
         private bool isNeedPreDownloadingICC = false;//Added 02/13 by Bruce
 
+        private static string uSerid = string.Empty;
         #endregion
 
         #region Constructor
@@ -726,6 +727,9 @@ namespace DDPM.SA.Plugins.User.DeviceManager
             thread.Start();
 
             _disDevHelper = new DisplayDeviceHelper(Log);
+
+            uSerid = WTSFunction.DirectGetUserID(Log);
+            writelog("DeviceManagerPlugin uSerid ...");
         }
 
         private void OnCurrentSessionInactived(object sender, EventArgs e)
@@ -20142,6 +20146,11 @@ namespace DDPM.SA.Plugins.User.DeviceManager
         public async Task<bool> GetDTPProxyPluginReady()
         {
             return _DTPProxyPlugin.GetDTPProxyPluginReady().Result;
+        }
+
+        public Task<string> GetUserID()
+        {
+            return Task.FromResult(uSerid);
         }
     }
 }
