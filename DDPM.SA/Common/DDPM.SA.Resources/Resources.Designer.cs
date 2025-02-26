612e88a72a0627b2807946e1ecd5814c7d43cb35
brucechuang <brucechuang@wits.com>
2025-02-26 15:13:39 +0800

    [SA] 1.Add "."

--- DDPM.SA/Common/DDPM.SA.Resources/Resources.Designer.cs
+++ DDPM.SA/Common/DDPM.SA.Resources/Resources.Designer.cs
@@ -1168,7 +1168,7 @@ namespace DDPM.SA.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Updating firmware. Do not remove or power off the device. Leave the device undisturbed.
+        ///   Looks up a localized string similar to Updating firmware. Do not remove or power off the device. Leave the device undisturbed..
         /// </summary>
         internal static string Updating_firmware_Do_not_remove_or_power_off_the_device_Leave_the_device_undisturbed {
             get {
