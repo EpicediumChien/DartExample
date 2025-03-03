78ee3a8745d5e581a22d3151c5cbc5c8e6174344
Sheng-Chieh Chou <jc800703@hotmail.com>
2025-03-03 19:41:37 +0800

    [SA] Fix duplicated code

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
@@ -18147,9 +18147,20 @@ namespace DDPM.SA.Plugins.User.DeviceManager
 
                                     case OSDType.Error:
                                         {
-                                            // if-else do same thing? 
-                                            if (State) 
-                                            {
+                                            //if (State) 
+                                            //{
+                                            //    try
+                                            //    {
+                                            //        _OSD_Controler.Error_CloseWindow();
+                                            //        _OSD_Controler.Error_ShowWindow(title, Content, stayOpen, (sreen.WorkingArea.Top / (double)dpiX), (sreen.WorkingArea.Left / (double)dpiX));
+                                            //    }
+                                            //    catch (Exception ex)
+                                            //    {
+                                            //        writelog($"[_showosd] ERROR - OSDType.Error: {ex.Message}, State:{State}");
+                                            //    }
+                                            //}
+                                            //else
+                                            //{
                                                 try
                                                 {
                                                     _OSD_Controler.Error_CloseWindow();
@@ -18159,19 +18170,7 @@ namespace DDPM.SA.Plugins.User.DeviceManager
                                                 {
                                                     writelog($"[_showosd] ERROR - OSDType.Error: {ex.Message}, State:{State}");
                                                 }
-                                            }
-                                            else
-                                            {
-                                                try
-                                                {
-                                                    _OSD_Controler.Error_CloseWindow();
-                                                    _OSD_Controler.Error_ShowWindow(title, Content, stayOpen, (sreen.WorkingArea.Top / (double)dpiX), (sreen.WorkingArea.Left / (double)dpiX));
-                                                }
-                                                catch (Exception ex)
-                                                {
-                                                    writelog($"[_showosd] ERROR - OSDType.Error: {ex.Message}, State:{State}");
-                                                }
-                                            }
+                                            //}
                                         }
                                         break;
 
