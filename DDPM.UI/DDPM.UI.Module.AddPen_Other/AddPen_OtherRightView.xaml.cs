8a0273301efdefc2292d539d2945bb5967ab7813
Hess Cheng <HessCheng@wits.com>
2025-03-04 16:34:49 +0800

    update MessageModalDialog

--- DDPM.UI/DDPM.UI.Module.AddPen_Other/AddPen_OtherRightView.xaml.cs
+++ DDPM.UI/DDPM.UI.Module.AddPen_Other/AddPen_OtherRightView.xaml.cs
@@ -107,7 +107,7 @@ namespace DDPM.UI.Module.AddPen_Other
                 }
                 else
                 {
-                    messageModalDialog = new(Strings.PairYourPen, Strings.PairYourPenMessage, Strings.Yes, Strings.No);
+                    messageModalDialog = new(Strings.PairYourPen, Strings.PairYourPenMessage, Strings.No, Strings.Yes);
                 }
                 if (mainWindow != null)
                 {
