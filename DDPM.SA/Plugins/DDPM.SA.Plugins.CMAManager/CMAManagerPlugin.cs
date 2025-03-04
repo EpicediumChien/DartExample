2a2af080e8e59532185afd95d51386019ec6c52a
stephen <stephen@wits.com>
2025-03-04 20:05:33 +0800

    fix defer timer define for official

--- DDPM.SA/Plugins/DDPM.SA.Plugins.CMAManager/CMAManagerPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.CMAManager/CMAManagerPlugin.cs
@@ -1358,8 +1358,8 @@ namespace DDPM.SA.Plugins.CMAManager
         private const int INTERVAL_CHECK_SECONDS = 10 * 60 * 1000; // 10 mins*/
 
         // test
-        private const long DAY_IN_SECONDS = 3 * 60;    // 24 hours
-        private const int INTERVAL_CHECK_SECONDS = 5 * 60 * 1000; // 10 mins
+        private const long DAY_IN_SECONDS = 24 * 60 * 60;    // 24 hours
+        private const int INTERVAL_CHECK_SECONDS = 10 * 60 * 1000; // 10 mins
         private System.Timers.Timer timerDefer;
 
         private void initDeferControlPanel()
