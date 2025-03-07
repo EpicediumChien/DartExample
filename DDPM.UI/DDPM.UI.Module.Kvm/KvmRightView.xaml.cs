fd34cf1a93e29cbed24b4558492cb7a277a91b34
JasonLin <jasonlin3@wits.com>
2025-03-07 11:27:04 +0800

    [UI]modified open USBKVM

--- DDPM.UI/DDPM.UI.Module.Kvm/KvmRightView.xaml.cs
+++ DDPM.UI/DDPM.UI.Module.Kvm/KvmRightView.xaml.cs
@@ -158,17 +158,14 @@ namespace DDPM.UI.Module.Kvm
                     //vm.SetPXP = Visibility.Collapsed;
                     //vm.EditInput = Visibility.Visible;
                     //vm.EditPXP = Visibility.Visible;
-                    InputSourceFullView _inputSourceFullView = new InputSourceFullView();
-                    _inputSourceFullView.DataContext = vm;
-                    DdpmCommonHelper.ModuleOwner?.OpenFullView(_inputSourceFullView);
                 }
                 else
                 {
                     vm.Invoke_USBKVM();
-                    InputSourceFullView _inputSourceFullView = new InputSourceFullView();
-                    _inputSourceFullView.DataContext = this;
-                    DdpmCommonHelper.ModuleOwner?.OpenFullView(_inputSourceFullView);
                 }
+                InputSourceFullView _inputSourceFullView = new InputSourceFullView();
+                _inputSourceFullView.DataContext = vm;
+                DdpmCommonHelper.ModuleOwner?.OpenFullView(_inputSourceFullView);
             }
         }
 
