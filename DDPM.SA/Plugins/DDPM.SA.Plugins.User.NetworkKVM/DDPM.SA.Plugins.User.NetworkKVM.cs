0bec8f5d4e3656e2b7160cf5799dc558cb454243
Sheng-Chieh Chou <jc800703@hotmail.com>
2025-03-14 16:59:30 +0800

    [SA/DDPM.SA.Plugins.User.NetworkKVM.cs] Information Exposure Through an Error Message

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.NetworkKVM/DDPM.SA.Plugins.User.NetworkKVM.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.NetworkKVM/DDPM.SA.Plugins.User.NetworkKVM.cs
@@ -36,6 +36,19 @@ namespace NetworkKVM.Plugins
     [PublishedUnelevatedInterface(new[] { typeof(INKVMService) })]
     public class NKVMPlugin : BaseAgentPlugin, INKVMService, IDisposableObservable
     {
+        private static void WriteLog(ILog log, string message, bool isError = false)
+        {
+#if DEBUG
+            Console.WriteLine(message);
+#endif
+            if (log == null)
+                return;
+            if (!isError)
+                log.Info(message);
+            else
+                log.Error(message);
+        }
+
         #region Private Members
 
         private const string pluginName = "NKVMPlugin";
@@ -1211,7 +1224,8 @@ namespace NetworkKVM.Plugins
                                 }
                                 catch (Exception ex)
                                 {
-                                    _logs.DebugMsg($"[NetworkKVM] Failed to connect {ex}");
+                                    //_logs.DebugMsg($"[NetworkKVM] Failed to connect {ex}");
+                                    WriteLog(Log, $"[NetworkKVM] Failed to connect {ex.Message}", true);
                                     Disconnect();
                                     Thread.Sleep(1000);
                                     _AllInfoMonitors = GetMonitors().Result;
@@ -1326,7 +1340,8 @@ namespace NetworkKVM.Plugins
                                 }
                                 catch (Exception ex)
                                 {
-                                    _logs.DebugMsg($"[NetworkKVM] Failed to connect {ex}");
+                                    //_logs.DebugMsg($"[NetworkKVM] Failed to connect {ex}");
+                                    WriteLog(Log, $"[NetworkKVM] Failed to connect {ex.Message}", true);
                                     Disconnect();
                                     Thread.Sleep(1000);
                                     _AllInfoMonitors = GetMonitors().Result;
