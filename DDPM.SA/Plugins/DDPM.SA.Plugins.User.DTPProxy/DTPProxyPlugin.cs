432922e10aa7ce82c4f33b439dfa1d09146844d4
Wayn_chen <Wayn_chen@wistron.com>
2025-03-19 16:04:58 +0800

    Add log

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.DTPProxy/DTPProxyPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.DTPProxy/DTPProxyPlugin.cs
@@ -15640,7 +15640,7 @@ namespace DDPM.SA.Plugins.User.DTPProxy
             }
             catch (Exception e)
             {
-                writelog($"[AirAudio] {index} RegisterEventsForHeadset Exception {e.Message}");
+                writelog($"[AirAudio] {index} RegisterEventsForAirAudioAsync Exception {e.Message}");
 
                 return false;
             }
