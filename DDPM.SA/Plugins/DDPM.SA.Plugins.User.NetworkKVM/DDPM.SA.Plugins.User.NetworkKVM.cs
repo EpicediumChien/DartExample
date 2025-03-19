2303cb0c1e7bb6a84294b5aef31b9e3ac80b03cb
Sheng-Chieh Chou <jc800703@hotmail.com>
2025-03-19 17:29:13 +0800

    [SA/DDPM.SA.Plugins.User.NetworkKVM.cs] Improper Exception Handling

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.NetworkKVM/DDPM.SA.Plugins.User.NetworkKVM.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.NetworkKVM/DDPM.SA.Plugins.User.NetworkKVM.cs
@@ -1551,14 +1551,15 @@ namespace NetworkKVM.Plugins
             {
                 if (pipeServer.IsConnected)
                 {
-                    //try
-                    //{
-                    await pipeServer.WriteAsync(buffer, 0, buffer.Length).ConfigureAwait(false);
-                    //}
-                    //catch (Exception ex)
-                    //{
-                    //    _logs.DebugMsg("[NetworkKVM] WriteAsync exception, message: " + ex.Message);
-                    //}
+                    try
+                    {
+                        await pipeServer.WriteAsync(buffer, 0, buffer.Length).ConfigureAwait(false);
+                    }
+                    catch (Exception ex)
+                    {
+                        //_logs.DebugMsg("[NetworkKVM] WriteAsync exception, message: " + ex.Message);
+                        WriteLog($"WriteAsync exception, message: {ex.Message}", log_type.error);
+                    }
 
                     await pipeServer.FlushAsync();
                     //pipeServer.WaitForPipeDrain();
@@ -1587,16 +1588,16 @@ namespace NetworkKVM.Plugins
             {
                 if (pipeServer.IsConnected)
                 {
-                    //try
-                    //{
-                    bytesRead = await pipeServer.ReadAsync(buffer, 0, buffer.Length).ConfigureAwait(false);
-
-                    //}
-                    //catch (Exception ex) 
-                    //{ 
-                    //    _logs.DebugMsg("[NetworkKVM] ReadAsync failed, message: " + ex.Message);
-                    //    return string.Empty;
-                    //}
+                    try
+                    {
+                        bytesRead = await pipeServer.ReadAsync(buffer, 0, buffer.Length).ConfigureAwait(false);
+                    }
+                    catch (Exception ex) 
+                    { 
+                        //_logs.DebugMsg("[NetworkKVM] ReadAsync failed, message: " + ex.Message);
+                        WriteLog($"ReadAsync failed, message: " + ex.Message, log_type.error);
+                        return string.Empty;
+                    }
 
                     readmessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
                     _logs.DebugMsg("[NetworkKVM] ReadAsync : " + readmessage);
