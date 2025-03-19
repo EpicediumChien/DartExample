f6ab3a1162eccc2787509b14be58c5d154cd2ff0
Sheng-Chieh Chou <jc800703@hotmail.com>
2025-03-19 18:21:26 +0800

    [SA] Improper Exception Handling

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.NetworkKVM/DDPM.SA.Plugins.User.NetworkKVM.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.NetworkKVM/DDPM.SA.Plugins.User.NetworkKVM.cs
@@ -36,6 +36,37 @@ namespace NetworkKVM.Plugins
     [PublishedUnelevatedInterface(new[] { typeof(INKVMService) })]
     public class NKVMPlugin : BaseAgentPlugin, INKVMService, IDisposableObservable
     {
+        public enum log_type
+        {
+            info = 0,
+            error
+        }
+        /// <summary>
+        /// //
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="log_type">0 means info, others means error</param>
+        private void WriteLog(string text, log_type log_type = log_type.info,
+            [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+            [System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "",
+            [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0)
+        {
+            if (string.IsNullOrEmpty(text))
+                text = "";
+
+            text = $"[NKVMPlugin] {text}, Caller Name:{memberName}, Source Line {sourceLineNumber}";
+#if DEBUG
+            Console.WriteLine(text);
+#endif
+            if (Log != null)
+            {
+                if (log_type == log_type.info)
+                    Log.Info(text);
+                else
+                    Log.Error(text);
+            }
+        }
+
         #region Private Members
 
         private const string pluginName = "NKVMPlugin";
