763184d02931a6317f04caf5398b71b0cb8e1a36
Sheng-Chieh Chou <jc800703@hotmail.com>
2025-03-20 18:05:15 +0800

    [SA/Download.cs] Improper Resource Shutdown or Release

--- DDPM.SA/Common/DDPM.SA.Common/Method/Download.cs
+++ DDPM.SA/Common/DDPM.SA.Common/Method/Download.cs
@@ -150,8 +150,11 @@ namespace DDPM.SA.Common.Method
             {
                 FailInfo = "Network fail";
                 _logs?.DebugMsg_1(nameof(DownloadFile_OnLocal) + " fail:" + ex.ToString());
-                DownloadFileStream.Close();
                 return false;
+            }
+            finally
+            {
+                DownloadFileStream.Close();
             }
         }
         public double GetProgress()
