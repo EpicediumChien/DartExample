6d3c16e2365db6418756a51855dd6d979b55560b
stephen <stephen@wits.com>
2025-03-28 13:44:56 +0800

    add error handle

--- DDPM.SA/Common/DDPM.SA.Common/Defer/FwJobControlPanel.cs
+++ DDPM.SA/Common/DDPM.SA.Common/Defer/FwJobControlPanel.cs
@@ -207,7 +207,14 @@ namespace DDPM.SA.Common.Defer
                 }
             }
 
-            writeToFile();
+            try
+            {
+                writeToFile();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("writeToFile() exception: " + ex.ToString());
+            }
 
             return deferItems;
 
