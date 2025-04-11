bbb4db86f25f003e15a2b5ca79a658b89569e854
Rex_Cai@wistron.com <Rex_Cai@wistron.com>
2025-04-11 09:50:37 +0800

    PIMS-345995 SWUpdate response

--- DDPM.SA/Plugins/CLI.Plugins.Peripherals/CLIPeripheralsPlugins.cs
+++ DDPM.SA/Plugins/CLI.Plugins.Peripherals/CLIPeripheralsPlugins.cs
@@ -3977,6 +3977,7 @@ namespace DDPM.CLI.Plugins.Peripherals
                                 cLI_SWU_RESPONSE.SWVersion = $"[{_GlobalSettingParam.GlobalSetting_About.SWVersion}]";
                                 cLI_SWU_RESPONSE.Message = "No update availible";
                                 cLI_SWU_RESPONSE.Result = "PASS";
+                                cLI_SWU_RESPONSE.SWUpdateRESPONSE = [$"No updates available: DDPM already last Version:{_GlobalSettingParam.GlobalSetting_About.SWVersion}"];
                             }
                         }
                         //else if (commandLineInput.Options.Count == 0)
