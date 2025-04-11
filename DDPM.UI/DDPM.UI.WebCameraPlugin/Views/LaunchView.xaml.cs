996df2488be05656eddedd9f9eab319bc3f062b8
Kidd Huang <KiddHuang@wits.com>
2025-04-11 09:56:31 +0800

    [Fix] Fix Fix PIMS-321013, PIMS-325273, PIMS-344423, PIMS-345698, PIMS-345797, PIMS-348097, PIMS-349314, PIMS-349317 For P series and U series USB2.0

--- DDPM.UI/DDPM.UI.WebCameraPlugin/Views/LaunchView.xaml.cs
+++ DDPM.UI/DDPM.UI.WebCameraPlugin/Views/LaunchView.xaml.cs
@@ -614,12 +614,12 @@ namespace DDPM.UI.Plugin.WebCameraPlugin
             string model = _vm.CurrentDeviceInfo?.ModelNumber ?? "";
             noPresenceFunction = false;
 
-            //if (is_camera_dell7==2 && !AllSupportedResolutions)
-            //{
-            //    print_debug("is_camera_dell7 ==2 && !AllSupportedResolutions");
-            //    noPresenceFunction = true;
-            //    return;
-            //}
+            if (!AllSupportedResolutions)
+            {
+                print_debug("is_camera_dell7 ==2 && !AllSupportedResolutions");
+                noPresenceFunction = true;
+                return;
+            }
             _vm.UPD_Visibility = Visibility.Collapsed; //HPD
             _vm.MPS_Setting_Visibility = Visibility.Collapsed;//內建電腦MPS
             _vm.MPS_UpdateFW_Visibility = Visibility.Collapsed;//FW Update
