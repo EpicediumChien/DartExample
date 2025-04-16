642d2711da3a1a1f7ecfa220b11d33f7405f7d58
JasonLin <jasonlin3@wits.com>
2025-04-16 11:31:56 +0800

    [SA][20250416]modified InitDisplayData due to timing

--- DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
+++ DDPM.SA/Plugins/DDPM.SA.Plugins.User.DeviceManager/DeviceManagerPlugin.cs
@@ -12322,6 +12322,8 @@ namespace DDPM.SA.Plugins.User.DeviceManager
                             writelog($"[DeviceMangerPlugin] SystemEventsDisplaySettingsChangedAsync() get monitor count {NewMonitors.Count} ...");
 
                             var T1 = Task.Run(() => InitMonitorSettings(NewMonitors.ToList(), token), token);
+                            //04/16 Jason Lin add InitDisplayData due to timing
+                            _DisplayManagerPlugin.InitDisplayData(NewMonitors.ToList()).Wait(token);
                             var T2 = Task.Run(() => InitAllDisplayData(NewMonitors.ToList(), token), token);
 
                             // add @ 20250303 stephen
@@ -13055,7 +13057,7 @@ namespace DDPM.SA.Plugins.User.DeviceManager
             {
                 if (AllMonitors != null && AllMonitors.Count > 0)
                 {
-                    _DisplayManagerPlugin.InitDisplayData(AllMonitors).Wait(cancellationToken);
+                    //_DisplayManagerPlugin.InitDisplayData(AllMonitors).Wait(cancellationToken);
 
                     for (int i = 0; ((i < AllMonitors.Count) && (!cancellationToken.IsCancellationRequested)); i++)
                     {
@@ -13107,7 +13109,8 @@ namespace DDPM.SA.Plugins.User.DeviceManager
             writelog($"monitor count {e.monitors.Count} ...");
 
             InitMonitorSettings((e.monitors).ToList(), CancellationToken.None);
-
+            //04/16 Jason Lin add InitDisplayData due to timing
+            _DisplayManagerPlugin.InitDisplayData((e.monitors).ToList()).Wait(CancellationToken.None);
             Task.Run(() => InitAllDisplayData((e.monitors).ToList(), CancellationToken.None));
 
             DisplaychangedEventArgs _displaychangedEventArgs = new DisplaychangedEventArgs();
