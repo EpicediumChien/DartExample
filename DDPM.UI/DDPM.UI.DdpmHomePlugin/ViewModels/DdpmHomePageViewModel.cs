34e8da68b3ac316521ce7888f36ac8de8c57a52a
Wayn_chen <Wayn_chen@wistron.com>
2025-04-18 09:06:10 +0800

    Update

--- DDPM.UI/DDPM.UI.DdpmHomePlugin/ViewModels/DdpmHomePageViewModel.cs
+++ DDPM.UI/DDPM.UI.DdpmHomePlugin/ViewModels/DdpmHomePageViewModel.cs
@@ -596,11 +596,12 @@ namespace DDPM.UI.Plugin.DdpmHomePlugin.ViewModels
                             dev.InstanceNo = instanceNo;
                         }
 
-                        //_homeDevices.Add(dev);
-                        if (GlobalDefinitions.isSupport210 && devType.ToString().ToUpper().Contains("25"))
-                            tempList.Add(dev);
-                        else
+                        if (!GlobalDefinitions.isSupport210 && devType.ToString().ToUpper().Contains("25"))
                             return;
+                        //_homeDevices.Add(dev);
+                        tempList.Add(dev);
+
+
                     }
                     //OnPropertyChanged("HomeDevices");
 
